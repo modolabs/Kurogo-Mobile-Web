3ff649ce4ca7b9cea685271e69b35725c8891a4a
Muhammad <muhammad.amjad@modolabs.com>
2010-07-12 17:18:20 -0400

    Some updates to the back-end Events/Calendar stuff

--- mobi-web/api/HarvardCalendar.php
+++ mobi-web/api/HarvardCalendar.php
@@ -1,6 +1,10 @@
 <?php
 
 require_once LIBDIR ."/harvard_ical_lib.php";
+require_once LIBDIR ."/harvard_calendar.php";
+
+define('PATH_TO_EVENTS_CAT', '../../mobi-lib/event_cat');
+
 
 define('HARVARD_EVENTS_ICS_BASE_URL', 'http://www.trumba.com/calendars/gazette.ics');
 
@@ -66,6 +70,10 @@ function makeICAL($icsURL, $dateString)
 	
 }
 
+
+
+
+
 switch ($_REQUEST['command']) {
   case 'day':
    $type = $_REQUEST['type'];
@@ -81,18 +89,55 @@ switch ($_REQUEST['command']) {
    }
 
     elseif ($type == 'Exhibits') {
-     $events = MIT_Calendar::TodaysExhibitsHeaders($date);
+    // $events = MIT_Calendar::TodaysExhibitsHeaders($date);
     }
-
-  // $events = makeICAL($url, $date);
 
    foreach ($events as $event) {
      $data[] = clean_up_ical_event($event);
    }
    break;
 
-  default:
+   case 'category':
+    if ($id = $_REQUEST['id']) {
+     $start = isset($_REQUEST['start']) ? $_REQUEST['start'] : time();
+     $end = isset($_REQUEST['end']) ? $_REQUEST['end'] : $start + 86400;
+     $start = date('Ymd', $start);
+     $end = date('Ymd', $end);
+
+     $url = HARVARD_EVENTS_ICS_BASE_URL ."?startdate=" .$start ."&filter1=" .$id ."&filterfield1=15202";
+
+     //$events = MIT_Calendar::HeadersByCatID($id, $start, $end);
+   
+
+   $events = makeICAL($url, $start);
+
+    foreach ($events as $event) {
+        $data[] = clean_up_ical_event($event);
+        }
+    }
    break;
+
+   case 'categories':
+    $categories = Harvard_Calendar::get_categories(PATH_TO_EVENTS_CAT);
+
+       foreach ($categories as $categoryObject) {
+     
+           $name = ucwords($categoryObject->get_name());
+           $catid = $categoryObject->get_cat_id();
+           $url = $categoryObject->get_url();
+
+           $catData = array('name' => $name,
+		      'catid' => $catid,
+                      'url' => $url);
+
+           $data[] = $catData;
+           }
+   break;
+
+   default:
+       break;
+
+ 
 }
 
 echo json_encode($data);
