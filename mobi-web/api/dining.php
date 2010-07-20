19877de2c86bc6048fb16bcbb0e92a72c44047c5
Muhammad <muhammad.amjad@modolabs.com>
2010-07-19 21:40:40 -0400

    A rough fix for the Dining Module "Hours" tab. Also some changes to the back-end Dining info-retrieval (but not fully fuctional yet).
    Please enter the commit message for your changes. Lines starting

--- mobi-web/api/dining.php
+++ mobi-web/api/dining.php
@@ -4,20 +4,34 @@
 
  $urlBase = 'http://food.cs50.net/api/1.1/items?date=';
 
+ $hours_indicator = 0;
+
  switch ($_REQUEST['command']) {
  
 	case 'breakfast':
 	     $mealTime = '&meal=Breakfast&output=json';
+            $hours_indicator = 0;
+            break;
 
 	case 'lunch':
 	     $mealTime = '&meal=Lunch&output=json';
+            $hours_indicator = 0;
+            break;
 
 	case 'dinner':
 	     $mealTime = '&meal=Dinner&output=json';
+            $hours_indicator = 0;
+            break;
+
+        case 'hours':          
+            $hours_indicator = 1;
+            echo json_encode(DINING_HOURS::getDiningHours());
+            break;
+
  }
 
+ if ($hours_indicator == 0)
  echo(HARVARD_DINING::getMealData($urlBase, $_REQUEST['date'], $mealTime)); 
-
 /*
 switch ($_REQEUST['command']) {
 
