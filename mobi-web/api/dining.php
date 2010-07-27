5ffd8ffc33d3b8ca2881f6cd7597f8b8a85a7b9e
Muhammad <muhammad.amjad@modolabs.com>
2010-07-26 20:17:28 -0400

    Dining Module Additions/Changes: 1. Temporarily placed the sample MENU file in the "static" folder 2. Added a UI Alert that prevents acessing dates one-week ahead or backwards in time 3. Made minor changes to MenuDetailsViewController.m and MenuItems.m to display the data retrieved from the sample-dining file instead of the CS50 API.

--- mobi-web/api/dining.php
+++ mobi-web/api/dining.php
@@ -3,36 +3,41 @@
  require_once LIBDIR . "/harvard_dining.php";
  require_once LIBDIR ."/diningHrs.php";
 
- $urlBase = 'http://food.cs50.net/api/1.1/items?date=';
+ //$urlBase = 'http://food.cs50.net/api/1.1/items?date=';
 
  $hours_indicator = 0;
 
  switch ($_REQUEST['command']) {
  
 	case 'breakfast':
-	     $mealTime = '&meal=Breakfast&output=json';
-            $hours_indicator = 0;
+	    $mealTime = 'BRK';
+            $day = isset($_REQUEST['date']) ? $_REQUEST['date'] : date('Y-m-d', time());
+            echo json_encode(DINING_DATA::getDiningData($day, $mealTime));
+            //$hours_indicator = 0;
             break;
 
 	case 'lunch':
-	     $mealTime = '&meal=Lunch&output=json';
-            $hours_indicator = 0;
+	    $mealTime = 'LUN';
+            $day = isset($_REQUEST['date']) ? $_REQUEST['date'] : date('Y-m-d', time());
+            echo json_encode(DINING_DATA::getDiningData($day, $mealTime));
+            //$hours_indicator = 0;
             break;
 
 	case 'dinner':
-	     $mealTime = '&meal=Dinner&output=json';
-            $hours_indicator = 0;
+	     $mealTime = 'DIN';
+            $day = isset($_REQUEST['date']) ? $_REQUEST['date'] : date('Y-m-d', time());
+            echo json_encode(DINING_DATA::getDiningData($day, $mealTime));
+            //$hours_indicator = 0;
             break;
 
         case 'hours':
-            $hours_indicator = 1;
             echo json_encode(DINING_HOURS::getDiningHours());
             break;
 
  }
 
- if ($hours_indicator == 0)
- echo(HARVARD_DINING::getMealData($urlBase, $_REQUEST['date'], $mealTime)); 
+// if ($hours_indicator == 0)
+ //echo(HARVARD_DINING::getMealData($urlBase, $_REQUEST['date'], $mealTime));
 /*
 switch ($_REQEUST['command']) {
 
