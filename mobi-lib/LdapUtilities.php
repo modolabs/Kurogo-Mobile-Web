81dc53e031405e05384d05ee6b22a762f84d99ba
David Ormsbee <david.ormsbee@modolabs.com>
2010-08-12 14:19:33 -0400

    fix for bug [#92], made directory search able to ignore middle names and titles when 3 or more terms are being searched on.

--- mobi-lib/LdapUtilities.php
+++ mobi-lib/LdapUtilities.php
@@ -4,14 +4,14 @@ require_once "ldap_config.php";
 
 // Scenarios:
 // 
-// A: 1 search term. Could be given name, surname, or email. Query:
+// A: 1 search term. Could be given name, surname, or email. Query for "hewitt":
 //
 //  (|
 //	  (|(givenName=hewitt*)(sn=hewitt*))
 //    (mail=hewitt*)
 //  )
 //
-// B: 2 search terms. Names could be in either order. Query:
+// B: 2 search terms. Names could be in either order. Query for "doug hall":
 //
 //	(|
 //	  (&(givenName=doug*)(sn=hall*))
@@ -19,11 +19,15 @@ require_once "ldap_config.php";
 //	  (mail=doug*hall*)
 //	)
 //
-// C: 3 or more search terms. Assume name order matches common name (cn). Query:
+// C: 3 or more search terms. Query for "prof susan chan tack": 
 //
 //  (|
-//    (cn=susan*chan*tack*)
-//    (mail=susan*chan*tack*)
+//    (|
+//  	(&(givenName=prof*)(sn=tack*))
+//  	(&(givenName=susan*)(sn=tack*))
+//  	(cn=prof*susan*chan*tack*)
+//    )
+//    (mail=prof*susan*chan*tack)
 //  )
 //
 // We've been having problems having too many matches returned, so we're 
@@ -31,9 +35,9 @@ require_once "ldap_config.php";
 //
 // 1. It's assumed that anything you write comes at the beginning of a name or
 //    email address.  So "ith" will not match against "Smith".
-// 2. If there are more than two names written, we fall back to the common name,
-//    which means it's assuming an ordering of first, middle, then last name, 
-//    and not trying out every possible combination.
+// 2. If there are more than two names written, we fall back to assuming an 
+//    ordering of first, middle, then last name, and not trying out every 
+//    possible combination.
 
 function sanitizeSearch($search)
 {
@@ -56,9 +60,20 @@ function queryForNames($names)
 		return "(&(givenName=$names[0]*)(sn=$names[1]*))" . "(&(givenName=$names[1]*)(sn=$names[0]*))";
 	}
 	elseif ($nameCount > 2) {
-		// Three or more names -- just string them all together with wildcards
+		$lastName = $names[$nameCount - 1];
+
+		// Maybe they're listed in the directory as First Last, but being 
+		// searched as First Middle Last
+		$firstLastQuery = "(&(givenName=$names[0]*)(sn=$lastName*))";
+		
+		// Maybe we're seeing Title First [...] Last, ignore the title
+		$omitTitleQuery = "(&(givenName=$names[1]*)(sn=$lastName*))";
+
+		// Kitchen sink -- just string them all together with wildcards
 		// and hope that it's a match on the common name.
-		return "(cn=" . implode("*", $names) . "*)";
+		$allPartsQuery = "(cn=" . implode("*", $names) . "*)";
+		
+		return "(|" . $firstLastQuery . $omitTitleQuery . $allPartsQuery . ")";
 	}
 }
 
@@ -74,7 +89,7 @@ function buildNameAndEmailLDAPQuery($search)
 	$query = "(|" . queryForNames($words) . queryForEmail($words) . ")";
 
 	// error_log("SAFE SEARCH: " . $safeSearch);
-	// error_log("QUERY: " . $query);
+	error_log("QUERY: " . $query);
 
 	// Put the gathered clauses in the person search template.
     $searchFilter = str_replace("%s", $query, NAME_SEARCH_FILTER);
