303d12b292c0eff710e87d64d841b839fc24b667
Brian Patt <brian.patt@modolabs.com>
2010-08-13 19:46:41 -0400

    show correct date
    - now show the correct date at the top

--- mobi-web/dining/index.php
+++ mobi-web/dining/index.php
@@ -6,21 +6,14 @@ require_once LIBDIR . "/diningHrs.php";
 function day_info($time, $offset=0) {
   $time += $offset * 24 * 60 * 60;
   return array(
-    "weekday"       => date('l', $time),
-    "month"         => date('F', $time),
-    "month_3Let"    => date('M', $time),
-    "day_num"       => date('j', $time),
-    "year"          => date('Y', $time),
-    "month_num"     => date('m', $time),
-    "day_3Let"      => date('D', $time),
-    "day_num_2dig"  => date('d', $time),
-    "date"          => date('Y/m/d', $time),
-    "time"          => strtotime(date("Y-m-d 12:00:00", $time))
+    "pretty"         => date("D M j", $time),
+    "arrows_format"  => date("D F j", $time),
+    "time"           => strtotime(date("Y-m-d 12:00:00", $time))
   );
 }
 
-function dayURL($day, $type) {
-  return "index.php?time={$day['time']}&type=$type";
+function dayURL($day) {
+  return "index.php?time={$day['time']}";
 }
 
 $time = isset($_REQUEST['time']) ? $_REQUEST['time'] : time();
