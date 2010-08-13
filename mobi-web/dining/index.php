b649011b5162fd1cf180053a8a09cf4ccb79b24b
Brian Patt <brian.patt@modolabs.com>
2010-08-13 19:33:58 -0400

    beggining mobile web for dining
    - show food items for webkit, still need to do locations tab

--- mobi-web/dining/index.php
+++ mobi-web/dining/index.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once LIBDIR . "/harvard_dining.php";
+require_once LIBDIR . "/diningHrs.php";
+
 function day_info($time, $offset=0) {
   $time += $offset * 24 * 60 * 60;
   return array(
@@ -20,14 +23,54 @@ function dayURL($day, $type) {
   return "index.php?time={$day['time']}&type=$type";
 }
 
-$time = $_REQUEST['time'];
+$time = isset($_REQUEST['time']) ? $_REQUEST['time'] : time();
+
+$day = date('Y-m-d', $time);
 
 $current = day_info($time);
 $next = day_info($time, 1);
 $prev = day_info($time, -1);
 
+
+$food_items = array(
+    "breakfast" => DINING_DATA::getDiningData($day, "BRK"),
+    "lunch" => DINING_DATA::getDiningData($day, "LUN"),
+    "dinner" => DINING_DATA::getDiningData($day, "DIN"),
+);
+
+foreach($food_items as $meal => $items) {
+    $food_items[$meal] = collectFoodByCategory($items);
+}
+
+$hour = intval(date('G'));
+
+if($hour < 12) {
+    $current_meal = "breakfast";
+} else if ($hour < 15) {
+    $current_meal = "lunch";
+} else {
+    $current_meal = "dinner";
+}
+
 require "$page->branch/index.html";
 
 $page->output();
 
+
+function collectFoodByCategory($items) {
+    $food_categories = array();
+
+    foreach($items as $item) {
+        // this is a hack for now just to fill in some of the data
+        $item["properties"] = array("vegetarian", "local");
+        
+        if(!array_key_exists($item[category], $food_categories)) {
+            $food_categories[$item[category]] = array($item);
+        } else {
+            $food_categories[$item[category]][] = $item;
+        }
+    }
+
+    return $food_categories;
+}
 ?>
