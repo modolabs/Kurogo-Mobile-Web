b934e89014639be0d1099a4bb89ec2be5607de40
David Ormsbee <david.ormsbee@modolabs.com>
2010-08-17 22:43:16 -0400

    fixed indentation in coursesTest.php

--- mobi-web/api/coursesTest.php
+++ mobi-web/api/coursesTest.php
@@ -1,9 +1,8 @@
 <?php
-
 require_once LIBDIR . '/testCourses.php';
 
- switch ($_REQUEST['command']) {
-  case 'courses':
+switch ($_REQUEST['command']) {
+case 'courses':
     $data = CourseData::get_schoolsAndCourses();
     break;
 
@@ -13,20 +12,21 @@ case 'subjectList':
     $data = CourseData::get_subjectsForCourse(str_replace('-other', '', $courseId), $courseGroup);
 
     if(isset($_REQUEST['checksum'])) {
-      $checksum = md5(json_encode($data));
-      if(isset($_REQUEST['full'])) {
-        $data = array('checksum' => $checksum, 'classes' => $data);
-      } else {
-        $data = array('checksum' => $checksum);
-      }
+        $checksum = md5(json_encode($data));
+        if(isset($_REQUEST['full'])) {
+            $data = array('checksum' => $checksum, 'classes' => $data);
+        }
+        else {
+            $data = array('checksum' => $checksum);
+        }
     }
     break;
  
- case 'term':
-        $data = array('term' => CourseData::get_term());
-     break;
+case 'term':
+    $data = array('term' => CourseData::get_term());
+    break;
 
- case 'subjectInfo':
+case 'subjectInfo':
     $subjectId = urldecode($_REQUEST['id']);
     $data = CourseData::get_subject_details($subjectId);
     if($data) {
@@ -36,21 +36,18 @@ case 'subjectList':
     }
     break;
 
-    case 'search':
-
+case 'search':
     $query = urldecode($_REQUEST['query']);
     $school = urldecode($_REQUEST['courseGroup']);
     $course = urldecode($_REQUEST['courseName']);
-
     $course = urlencode(str_replace('-other', '', $course));
     $school = urlencode(str_replace('-other', '', $school));
     $query = urlencode($query);
 
     $data = CourseData::search_subjects($query, str_replace('-other', '', $school), str_replace(' ', '+', $course));
-
     break;
+}
 
- }
+echo json_encode($data);
 
- echo json_encode($data);
 ?>
