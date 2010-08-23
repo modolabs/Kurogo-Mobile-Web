7c55194ee6bef96833e8023f3a8e63381c0e5a56
sonya huang <sonya@sonya-huangs-SuperMac.local>
2010-08-23 17:27:56 -0400

    use fmt=json for map search.  yay for speediness. remove regex now that invalid json is fixed.

--- mobi-lib/MapSearch.php
+++ mobi-lib/MapSearch.php
@@ -4,28 +4,22 @@ require_once 'ArcGISServer.php';
 
 function searchCampusMap($query) {
 
-    $locs = array('bldg', 'dept', 'lib', 'museum', 'room');
     $resultObj = new stdClass();
     $resultObj->results = array();
     $bldgIds = array();
 
-    foreach ($locs as $loc) {
-
-        $params = array(
-            'str' => $query,
-            'loc' => $loc,
-            );
-        
-        $url = MAP_SEARCH_URL . '?' . http_build_query($params);
-        $rawContent = file_get_contents($url);
-        $rawContent = preg_replace('/:\s*"?([\-\.\d]+[A-Z]*)"?\s*([,\}])/', ':"${1}"${2}', $rawContent);
-        $content = json_decode($rawContent);
-        
-        foreach ($content->results as $resultObj) {
-            if (!in_array($resultObj->bld_num, $bldgIds))
-                $bldgIds[] = $resultObj->bld_num;
-        }
-
+    $params = array(
+        'str' => $query,
+        'fmt' => 'json',
+        );
+    
+    $url = MAP_SEARCH_URL . '?' . http_build_query($params);
+    $rawContent = file_get_contents($url);
+    $content = json_decode($rawContent);
+    
+    foreach ($content->results as $resultObj) {
+        if (!in_array($resultObj->bld_num, $bldgIds))
+            $bldgIds[] = $resultObj->bld_num;
     }
 
     if ($bldgIds) {
@@ -35,12 +29,6 @@ function searchCampusMap($query) {
                 $resultObj->results[] = $result;
             }
         }
-    }
-
-    // if map.harvard.edu can't find anything
-    // we'll try ArcGIS directly
-    if (!$resultObj->results) {
-        $resultObj = ArcGISServer::search($query);
     }
 
     return $resultObj;
