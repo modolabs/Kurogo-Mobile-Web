19b00a27f48a2d799defc67ccb58ebb1c9f95955
Jim Kang <jim.kang@modolabs.com>
2010-08-25 17:54:08 -0400

    check_modules: Coding standards conformance changes.

--- test/check_modules.py
+++ test/check_modules.py
@@ -12,46 +12,40 @@ That way, messages from Twill are not interspersed with error reporting.
 """
 Set this to the server you want to use for these tests.
 """
-g_base_url = "http://localhost:8888"
-#g_base_url = "http://mobile-dev.harvard.edu"
-#g_base_url = "http://mobile-staging.harvard.edu/"
-#g_base_url = "http://m.harvard.edu"
+BASE_URL = "http://localhost:8888"
+#BASE_URL = "http://mobile-dev.harvard.edu"
+#BASE_URL = "http://mobile-staging.harvard.edu"
+#BASE_URL = "http://m.harvard.edu"
 
-# TODO: ALL CAPS
-g_mobileSafariUserAgent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_1_3 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7E18 Safari/528.16"
-g_touchPhoneUserAgent = "BlackBerry9530/4.7.0.167 Profile/MIDP-2.0 Configuration/CLDC-1.1 VendorID/102 UP.Link/6.3.1.20.0 BlackBerry9530/5.0.0.328 Profile/MIDP-2.1 Configuration/CLDC-1.1 VendorID/105"
-g_blackberryPlusUserAgent = "BlackBerry9630/4.7.1.40 Profile/MIDP-2.0 Configuration/CLDC-1.1 VendorID/104"
-g_basicPhoneUserAgent = "LG U880: LG/U880/v1.0"
+MOBILE_SAFARI_USER_AGENT = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_1_3 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7E18 Safari/528.16"
+TOUCH_PHONE_USER_AGENT = "BlackBerry9530/4.7.0.167 Profile/MIDP-2.0 Configuration/CLDC-1.1 VendorID/102 UP.Link/6.3.1.20.0 BlackBerry9530/5.0.0.328 Profile/MIDP-2.1 Configuration/CLDC-1.1 VendorID/105"
+BLACKBERRY_PLUS_USER_AGENT = "BlackBerry9630/4.7.1.40 Profile/MIDP-2.0 Configuration/CLDC-1.1 VendorID/104"
+BASIC_PHONE_USER_AGENT = "LG U880: LG/U880/v1.0"
 
 import unittest
-from twill import get_browser
-from twill.commands import *
 import re
 import urllib
+from twill import get_browser
+from twill.commands import *
 
-""" Utility functions. """
-def endWithSlash(string):
-    if not string.endswith('/'):
-        string += '/'
-    return string
 
 """ 
 The 'abstract' base class for the test cases.
 """
 class TestModuleBase(unittest.TestCase):
-    def __init__(self, moduleName, 
-                 userAgent=g_basicPhoneUserAgent, branch='Basic', platform='',
+    def __init__(self, module_name, 
+                 user_agent=BASIC_PHONE_USER_AGENT, branch='Basic', platform='',
                  methodName='runTest'):
         unittest.TestCase.__init__(self, methodName)
-        self.baseUrl = g_base_url
-        self.moduleName = moduleName
-        self.userAgent = userAgent
+        self.base_url = BASE_URL
+        self.module_name = module_name
+        self.user_agent = user_agent
         self.branch = branch
         self.platform = platform
                 
     def setUp(self):
         self.browser = get_browser()
-        self.browser.set_agent_string(self.userAgent)
+        self.browser.set_agent_string(self.user_agent)
         self.browser.clear_cookies()
 
 """
@@ -60,70 +54,65 @@ Class for testing module apis.
 class TestModuleAPI(TestModuleBase):
     
     """
-    moduleName: e.g. people, dining. Is used as the module argument in the 
+    module_name: e.g. people, dining. Is used as the module argument in the 
     API query.
-    apiArgumentDict: Arguments for the API query other than module.
+    api_argument_dict: Arguments for the API query other than module.
     apiResultCheckRegex: The test case will run this regex against the API 
     output to determine whether or not the call was successful.
     """
-    def __init__(self, moduleName, 
-                 apiArgumentDict = {}, apiResultCheckRegex = '*'):
-        TestModuleBase.__init__(self, moduleName)
-        self.apiArgumentDict = apiArgumentDict
-        self.apiResultCheckRegex = apiResultCheckRegex
+    def __init__(self, module_name, 
+                 api_argument_dict = {}, api_result_check_regex = '*'):
+        TestModuleBase.__init__(self, module_name)
+        self.api_argument_dict = api_argument_dict
+        self.api_result_check_regex = api_result_check_regex
 
     def runTest(self):
         self.test_api()
         
     def test_api(self):
-        self.hit_api_with_arguments(self.apiArgumentDict)
+        self.hit_api_with_arguments(self.api_argument_dict)
         self.assertEqual(self.browser.get_code(), 200)
         self.verify_api_results()
         
     def hit_api_with_arguments(self, argumentDict):
         if 'module' not in argumentDict:
-            argumentDict['module'] = self.moduleName
+            argumentDict['module'] = self.module_name
 
-        queryString = '' # todo: Look for urllib method for this.
-        for arg, val in argumentDict.iteritems():
-            if len(queryString) > 0:
-                queryString += '&'
-            queryString += (arg + '=' + val)
-
-        echo("Query: {}".format(endWithSlash(self.baseUrl) + 'api/?' + queryString))
-        self.browser.go(endWithSlash(self.baseUrl) + 'api/?' + queryString)
+        queryString = urllib.urlencode(argumentDict)
+        #echo("Query: {}".format(end_with_slash(self.base_url) + 'api/?' + queryString))
+        self.browser.go(self.base_url + '/api/?' + queryString)
             
     def verify_api_results(self):
-        self.assertRegexpMatches(self.browser.get_html(), self.apiResultCheckRegex,
-            'Could not find API result matching "{}".'.format(self.apiResultCheckRegex))
+        self.assertRegexpMatches(self.browser.get_html(), self.api_result_check_regex,
+            'Could not find API result matching "{}".'.format(self.api_result_check_regex))
 
 class TestModulePage(TestModuleBase):
 
     """
-    moduleName: people, dining. Is used to build the URLs for the page in the module.
-    userAgent: The user agent string to pass. Some user agent constants are defined 
+    module_name: people, dining. Is used to build the URLs for the page in the module.
+    user_agent: The user agent string to pass. Some user agent constants are defined 
     at the top of the file.
     branch: e.g. Basic, Touch. The branch the test case should expect to be shown 
-    when it uses the userAgent to browse the module.
+    when it uses the user_agent to browse the module.
     platform: The platform the test case should expect to be shown. e.g. bbplus.
-    contentCheckRegExesAndMessageDict: A dictionary whose keys are regexes that the 
+    content_check_dict: A dictionary whose keys are regexes that the 
     test case should run against the html returned by the module page. The values 
     are error messages to log when the regexes fail to match anything.
-    locationWithinModule: The location of the page to test relative to 
-    host/moduleName/. Leave as '' to test the index page.
+    location_within_module: The location of the page to test relative to 
+    host/module_name/. Leave as '' to test the index page.
     """
-    def __init__(self, moduleName, 
-                 userAgent=g_basicPhoneUserAgent, branch='Basic', platform='',
-                 contentCheckRegExesAndMessageDict = {}, locationWithinModule = ''):
-        TestModuleBase.__init__(self, moduleName, userAgent, branch, platform)
-        self.locationWithinModule = locationWithinModule
-        self.contentCheckRegExesAndMessageDict = contentCheckRegExesAndMessageDict
+    def __init__(self, module_name, 
+                 user_agent=BASIC_PHONE_USER_AGENT, branch='Basic', platform='',
+                 content_check_dict = {}, location_within_module = ''):
+        TestModuleBase.__init__(self, module_name, user_agent, branch, platform)
+        self.location_within_module = location_within_module
+        self.content_check_dict = content_check_dict
 
     def runTest(self):
         self.browser.go(self.get_page_url())
         self.assertEqual(self.browser.get_code(), 200, 
             '{}: The page at {} is not OK. It returned HTTP code: {}'.format(
-                self.moduleName, self.get_page_url(), self.browser.get_code()))
+                self.module_name, self.get_page_url(), self.browser.get_code()))
         self.verify_branch()
         self.verify_platform()
         self.verify_contents()
@@ -134,17 +123,17 @@ class TestModulePage(TestModuleBase):
         self.assertRegexpMatches(self.browser.get_html(), 
             '<!--\ Branch:\ "{}"'.format(self.branch),
             "{} is not displaying the {} branch for user agent {}.".format(
-            self.browser.get_url(), self.branch, self.userAgent))
+            self.browser.get_url(), self.branch, self.user_agent))
     
     def verify_platform(self):
         if self.platform:
             self.assertRegexpMatches(self.browser.get_html(), 
                 '<!--\ Platform:\ "{}"'.format(self.platform),
                 "{} is not displaying the {} platform for user agent {}.".format(
-                self.browser.get_url(), self.platform, self.userAgent))
+                self.browser.get_url(), self.platform, self.user_agent))
         
     def verify_contents(self):
-        for regex, message in self.contentCheckRegExesAndMessageDict.iteritems():
+        for regex, message in self.content_check_dict.iteritems():
             self.assertRegexpMatches(self.browser.get_html(), regex, message)
         
     def verify_images(self):
@@ -172,8 +161,7 @@ class TestModulePage(TestModuleBase):
                     
     # Test helper methods
     def get_page_url(self):
-        moduleURL = endWithSlash(endWithSlash(self.baseUrl) + self.moduleName)
-        return moduleURL + self.locationWithinModule
+        return self.base_url + '/' + self.module_name + self.location_within_module
 
 
 # Test suite functions
@@ -181,14 +169,14 @@ class TestModulePage(TestModuleBase):
 # Adds a standard group of tests to the test suite for the module. If 
 # you need different tests for each branch (Basic, Touch, etc.), create 
 # the TestModulePage objects directly.
-def add_page_tests_for_module(moduleName, suite, contentCheckDict):
-    suite.addTest(TestModulePage(moduleName, g_basicPhoneUserAgent, 
+def add_page_tests_for_module(module_name, suite, contentCheckDict):
+    suite.addTest(TestModulePage(module_name, BASIC_PHONE_USER_AGENT, 
         'Basic', '', contentCheckDict))
-    suite.addTest(TestModulePage(moduleName, g_touchPhoneUserAgent, 
+    suite.addTest(TestModulePage(module_name, TOUCH_PHONE_USER_AGENT, 
         'Touch', '', contentCheckDict))
-    suite.addTest(TestModulePage(moduleName, g_mobileSafariUserAgent, 
+    suite.addTest(TestModulePage(module_name, MOBILE_SAFARI_USER_AGENT, 
         'Webkit', '', contentCheckDict))
-    suite.addTest(TestModulePage(moduleName, g_blackberryPlusUserAgent, 
+    suite.addTest(TestModulePage(module_name, BLACKBERRY_PLUS_USER_AGENT, 
         'Basic', 'bbplus', contentCheckDict))
 
 # Builds the test suite.
@@ -197,7 +185,7 @@ def suite():
 
     # People    
     testSuite.addTest(TestModuleAPI('people', 
-        {'q': 'roger+brockett', 'command': 'search'}, 'Brockett'))        
+        {'q': 'roger brockett', 'command': 'search'}, 'Brockett'))        
     add_page_tests_for_module('people', testSuite, 
         {'<title>People</title>': 'Could not verify index title.'})
 
