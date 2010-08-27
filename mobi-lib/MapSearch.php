4a4f2f36257f8d8d5ea7d9be48e303dc97ae84d7
Muhammad <muhammad.amjad@modolabs.com>
2010-08-27 11:47:13 -0400

    Courses MW: retrieve locations for courses

--- mobi-lib/MapSearch.php
+++ mobi-lib/MapSearch.php
@@ -34,6 +34,39 @@ function searchCampusMap($query) {
     return $resultObj;
 }
 
+// search for courses
+function searchCampusMapForCourseLoc($query) {
+
+    $resultObj = new stdClass();
+    $resultObj->results = array();
+    $bldgIds = array();
+
+    $params = array(
+        'str' => $query,
+        'loc' => 'course',
+        );
+
+    $url = MAP_SEARCH_URL . '?' . http_build_query($params);
+    $rawContent = file_get_contents($url);
+    $content = json_decode($rawContent);
+
+    foreach ($content->results as $resultObj) {
+        if (!in_array($resultObj->bld_num, $bldgIds))
+            $bldgIds[] = $resultObj->bld_num;
+    }
+
+    if ($bldgIds) {
+        foreach ($bldgIds as $bldgId) {
+            $obj = ArcGISServer::getBldgByNumber($bldgId);
+            foreach ($obj->results as $result) {
+                $resultObj->results[] = $result;
+            }
+        }
+    }
+
+    return $resultObj;
+}
+
 
 
 
