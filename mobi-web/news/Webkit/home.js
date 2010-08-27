4ded51ce94a27862d2464b41b8d64249454844df
Alexandra Ellwood <alexandra.ellwood@modolabs.com>
2010-08-27 16:55:13 -0400

    [#323] Add ellipsis Javascript support to webkit platform.

--- mobi-web/news/Webkit/home.js
+++ mobi-web/news/Webkit/home.js
@@ -15,3 +15,14 @@ function toggleSearch() {
     }
     return false;
 }
+
+// Initalize the ellipsis event handlers
+clipWithEllipsis(function () {
+    var elems = [];
+    for (var i = 0; i < 100; i++) { // cap at 100 divs to avoid overloading phone
+        var elem = document.getElementById('ellipsis_'+i);
+        if (!elem) { break; }
+        elems[i] = elem;
+    }
+    return elems;
+});
