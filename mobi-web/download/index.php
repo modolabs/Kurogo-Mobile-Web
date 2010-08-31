f88193a5c401fe7c9c4339091675b07235d52952
Eric J. Kim <eric.kim@modolabs.com>
2010-08-31 12:23:22 -0400

    MW: Mockups and CSS+images for BB download

--- mobi-web/download/index.php
+++ mobi-web/download/index.php
@@ -19,8 +19,8 @@ $device_apps = Array(
   );
 
 $device_instructions = Array(
-  'bbplus' => 'On the next screen, click "Download", and "OK" or "Run" once the download is complete.',
-  'blackberry' => 'On the next screen, click "Download", and "OK" or "Run" once the download is complete.',
+  'bbplus' => 'On the next screen, click "Download", and "OK" or "Run" once the download is complete. Once you\'ve installed the shortcut, it will appear on your BlackBerry home screen or in the Downloads folder.',
+  'blackberry' => 'On the next screen, click "Download", and "OK" or "Run" once the download is complete. Once you\'ve installed the shortcut, it will appear on your BlackBerry home screen or in the Downloads folder.',
   );
 
 $download_items = Array(
