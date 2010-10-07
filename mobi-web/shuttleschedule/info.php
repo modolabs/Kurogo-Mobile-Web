56508f7c76d2889f95c92607529ef3dc21fad020
Brian Patt <brian.patt@modolabs.com>
2010-10-06 21:42:12 -0400

    the shuttle info tab and info page for mobile web on webkit phones

---
+++ mobi-web/shuttleschedule/info.php
@@ -0,0 +1,21 @@
+<?php
+$docRoot = getenv("DOCUMENT_ROOT");
+
+require_once LIBDIR . "/TranslocReader.php";
+
+// this populates the $infoItems data
+require "shuttle_info.inc";
+
+$id = $_REQUEST['id'];
+
+foreach($infoItems as $infoItem) {
+  if($infoItem['id'] == $id) {
+    $content = $infoItem['html'];
+    break;
+  }
+}
+
+require "$page->branch/info.html";
+$page->output();
+
+?>
