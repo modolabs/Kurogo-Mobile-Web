89e8235a55c72ad70e4a8d49d723586f42ce06fb
Pete Akins <pete.akins@modolabs.com>
2010-10-14 13:30:18 -0400

    added FULL_URL_BASE constant

--- Web-app/lib/initialize.php
+++ Web-app/lib/initialize.php
@@ -54,9 +54,9 @@ function Initialize(&$path=null) {
     }
   }
   define('URL_BASE', $urlBase);
+  define('FULL_URL_BASE', sprintf("http://%s%s", $_SERVER['HTTP_HOST'], URL_BASE));
 
   define('COOKIE_PATH', URL_BASE); // We are installed under URL_BASE
-
 
   //
   // Load configuration files
