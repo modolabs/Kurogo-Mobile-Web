138a27875b2817969644d286152d5cba93fcc171
Pete Akins <pete.akins@modolabs.com>
2010-10-26 09:51:09 -0400

    added stub for transit module

---
+++ future/templates/modules/transit/TransitModule.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once realpath(LIB_DIR.'/Module.php');
+
+class TransitModule extends Module {
+  protected $id = 'transit';
+  
+  protected function initialize() {
+
+  }
+
+  protected function initializeForPage() {
+  
+  }
+
+}
---
+++ future/templates/modules/transit/index.tpl
@@ -0,0 +1,5 @@
+{include file="findInclude:common/header.tpl"}
+
+Transit
+
+{include file="findInclude:common/footer.tpl"}
