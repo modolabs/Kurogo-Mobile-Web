53afc3e7c2beeae03d29e58e5cb7a96b5fdf4c43
Pete Akins <pete.akins@modolabs.com>
2011-01-14 16:25:57 -0500

    added assoc property (default true)

--- future/lib/JSONDataParser.php
+++ future/lib/JSONDataParser.php
@@ -2,9 +2,10 @@
 
 class JSONDataParser extends DataParser
 {
+    protected $assoc=true;
     public function parseData($data)
     {
-        return json_decode($data);
+        return json_decode($data, $this->assoc);
     }
 }
 
