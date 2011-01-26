5a4ab875ee168231abdec02a98eff8db4b181218
Pete Akins <pete.akins@modolabs.com>
2011-01-26 10:58:58 -0500

    allow referencing users by email

--- future/lib/PasswdAuthentication.php
+++ future/lib/PasswdAuthentication.php
@@ -12,7 +12,9 @@ class PasswdAuthentication extends AuthenticationAuthority
     private $userFile;
     private $groupFile;
     private $users = array();
+    private $userEmails = array();
     private $groups = array();
+    private $groupGIDs = array();
     
     protected function validUserLogins()
     {
@@ -28,9 +30,14 @@ class PasswdAuthentication extends AuthenticationAuthority
         $data = file_get_contents($this->userFile);
         $lines = explode(PHP_EOL, $data);
         $this->users = array();
+        $this->userEmails = array();
         foreach ($lines as $line) {
             if ($user = $this->parseUserLine($line)) {
                 $this->users[$user['userID']] = $user;
+                if ($user['email']) {
+                    $this->userEmails[$user['userID']] = $user['email'];
+                }
+
             }
         }
     }
@@ -38,21 +45,39 @@ class PasswdAuthentication extends AuthenticationAuthority
     private function parseUserLine($line)
     {
         $line = trim($line);
-        if (strlen($line)==0 || preg_match("/^#/", $line)) {
+
+        // ignore blank lines or lines with pound symbol first (comments)
+        if (strlen($line)==0 || preg_match("/^\s*#/", $line)) {
             return false;
         }
 
         $fields = explode(":", $line);
-        if (!count($fields)==3) {
-            return false;
+        $user = array(
+            'userID'=>'',
+            'email'=>'',
+            'md5'=>'',
+            'fullname'=>''
+        );
+        switch (count($fields))
+        {
+            case 4:
+                $user['fullname'] = trim($fields[3]);
+            case 3:
+                $user['email']=Validator::isValidEmail(trim($fields[2])) ? trim($fields[2]) : '';
+            case 2:
+                $user['userID']=trim($fields[0]);
+                $user['md5']=trim($fields[1]);
+                break;
+            default:
+                return false;
+        }        
+
+        /* some quick validation */        
+        if (strlen($user['userID'])==0 | !preg_match("/^[a-f0-9]{32}$/", $user['md5'])) {
+            error_log("Invalid user line: $line");
+            $user = false;
         }
         
-        $user = array(
-            'userID'=>$fields[0],
-            'md5'=>$fields[1],
-            'email'=>$fields[2]
-        );
-
         return $user;
     }
 
@@ -69,9 +94,11 @@ class PasswdAuthentication extends AuthenticationAuthority
         $data = file_get_contents($this->groupFile);
         $lines = explode(PHP_EOL, $data);
         $this->groups = array();
+        $this->groupsByGID = array();
         foreach ($lines as $line) {
             if ($group = $this->parseGroupLine($line)) {
                 $this->groups[$group['group']] = $group;
+                $this->groupsGIDS[$group['group']] = $group['gid'];
             }
         }
     }
@@ -79,7 +106,9 @@ class PasswdAuthentication extends AuthenticationAuthority
     private function parseGroupLine($line)
     {
         $line = trim($line);
-        if (strlen($line)==0 || preg_match("/^#/", $line)) {
+
+        // ignore blank lines or lines with pound symbol first (comments)
+        if (strlen($line)==0 || preg_match("/^\s*#/", $line)) {
             return false;
         }
 
@@ -89,12 +118,14 @@ class PasswdAuthentication extends AuthenticationAuthority
         }
         
         $group = array(
-            'group'=>$fields[0],
-            'gid'=>$fields[1],
-            'members'=>explode(",",$fields[2])
+            'group'=>trim($fields[0]),
+            'gid'=>trim($fields[1]),
+            'members'=>array_map('trim', explode(",",trim($fields[2])))
         );
-        foreach ($group['members'] as $i=>$member) {
-            $group['members'][$i] = trim($member);
+        
+        if (strlen($group['group'])==0 || strlen($group['gid'])==0) {
+            error_log("Invalid group line: $line");
+            $group = false;
         }
 
         return $group;
@@ -106,9 +137,8 @@ class PasswdAuthentication extends AuthenticationAuthority
             return AUTH_FAILED;
         }
         
-        $this->loadUserData();
-        if (isset($this->users[$login])) {
-            if (md5($password) == $this->users[$login]['md5']) {
+        if ($userData = $this->getUserData($login)) {
+            if (md5($password) == $userData['md5']) {
                 $user = $this->getUser($login);
                 return AUTH_OK;
             } else {
@@ -118,6 +148,30 @@ class PasswdAuthentication extends AuthenticationAuthority
             return AUTH_USER_NOT_FOUND;
         }
     }
+    
+    /*
+     * Retrieves the user array by userID or email
+     * @param string $login a userID or email address
+     * @return array an array of userData or false if the user could not be found
+     */
+    private function getUserData($login)
+    {
+        if (strlen($login)==0) {
+            return false;
+        }
+        
+        $this->loadUserData();
+        
+        if (isset($this->users[$login])) {
+            return $this->users[$login];
+        }
+        
+        if (Validator::isValidEmail($login) && (($userID = array_search($login, $this->userEmails)) !== false)) {
+            return $this->getUserData($userID);
+        } 
+        
+        return false;
+    }
 
     public function getUser($login)
     {
@@ -125,15 +179,15 @@ class PasswdAuthentication extends AuthenticationAuthority
             return false;
         }
 
-        $this->loadUserData();
-        if (empty($login)) {
+        if (strlen($login)==0) {
             return new AnonymousUser();       
         }
 
-        if (isset($this->users[$login])) {
+        if ($userData = $this->getUserData($login)) {
             $user = new BasicUser($this);
-            $user->setUserID($this->users[$login]['userID']);
-            $user->setEmail($this->users[$login]['email']);
+            $user->setUserID($userData['userID']);
+            $user->setEmail($userData['email']);
+            $user->setFullName($userData['fullname']);
             return $user;
         } else {
             return false;
@@ -141,28 +195,52 @@ class PasswdAuthentication extends AuthenticationAuthority
         }
     }
 
-    public function getGroup($groupName)
+    private function getGroupData($group)
     {
-        $this->loadGroupData();
-        if (strlen($groupName)==0) {
+        if (strlen($group)==0) {
             return false;
         }
+        
+        $this->loadGroupData();
+        
+        if (isset($this->groups[$group])) {
+            return $this->groups[$group];
+        }
+        
+        if (($group = array_search($group, $this->groupGIDs) !== false)) {
+            return $this->getGroupData($group);
+        } 
+        
+        return false;
+    }
 
-        if (isset($this->groups[$groupName])) {
+    public function getGroup($group)
+    {
+        if (strlen($group)==0) {
+            return false;
+        }
+        
+        if ($groupData = $this->getGroupData($group)) {
+
             $group = new BasicUserGroup($this);
-            $group->setGroupID($this->groups[$groupName]['gid']);
-            $group->setGroupName($this->groups[$groupName]['group']);
+            $group->setGroupID($groupData['gid']);
+            $group->setGroupName($groupData['group']);
+            $members = is_array($groupData['members']) ? $groupData['members'] : array();
             
-            $members = is_array($this->groups[$groupName]['members']) ? $this->groups[$groupName]['members'] : array();
             $groupMembers = array();
+
+            //translate the user strings into user objects            
             foreach ($members as $user) {
+                //parse the authority|user pair. If no authority is indicated than use this authority's index
                 $userPieces = explode("|", $user);
-                $authority = count($userPieces)==2 ? $userPieces[0] : $this->getAuthorityIndex();
+                $authorityIndex = count($userPieces)==2 ? $userPieces[0] : $this->getAuthorityIndex();
                 $userID = count($userPieces)==2 ? $userPieces[1] : $userPieces[0];
-                if ($authority = AuthenticationAuthority::getAuthenticationAuthority($authority)) {
+                if ($authority = AuthenticationAuthority::getAuthenticationAuthority($authorityIndex)) {
                     if ($user = $authority->getUser($userID)) {
                         $groupMembers[] = $user;
                     }
+                } else {
+                    throw new Exception("Invalid authority $authorityIndex when parsing group information for $group");
                 }
             }
             $group->setMembers($groupMembers);
