903f037df33c69df7743a23eb9cc2f67b406c36e
Pete Akins <pete.akins@modolabs.com>
2011-02-13 09:23:30 -0500

    updated DayRange to include ending date (for multi-day all day events)

--- lib/DateTimeUtils.php
+++ lib/DateTimeUtils.php
@@ -184,15 +184,16 @@ class DayRange extends TimeRange {
         
         return $string;
     }
-  public function __construct($time, $tzid=NULL) {
-    
+  public function __construct($start, $end=null, $tzid=NULL) {
+    if (is_null($end)) {
+        $end = $start;
+    }
     // use mktime which uses system time zone if tzid is blank or is the same as system time zone TODO: what happens if it's different?
     if (!$tzid || $tzid == date_default_timezone_get()) {
-        $this->start = mktime(0, 0, 0, date('m', $time), date('d', $time), date('Y', $time));
-        $this->end = mktime(23, 59, 59, date('m', $time), date('d', $time), date('Y', $time));
+        $this->start = mktime(0, 0, 0, date('m', $start), date('d', $start), date('Y', $start));
+        $this->end = mktime(23, 59, 59, date('m', $end), date('d', $end), date('Y', $end));
     } else {
-        print_r(debug_backtrace());
-        die($tzid);
+        throw new Exception("Timezone set ($tzid), but is not the same as system time zone (" . date_default_timezone_get() . "). This case needs to be handled");
     }
   }
 }
