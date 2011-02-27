278aae97de0d605264af55ece21bef1584d32b5f
Brian Patt <brian.patt@modolabs.com>
2011-02-27 14:10:48 -0500

    fixing the diskCache readImage method, I suspect this method was never used before, but it should work now.

--- lib/DiskCache.php
+++ lib/DiskCache.php
@@ -98,13 +98,13 @@ class DiskCache {
     $suffix = $this->suffix ? $this->suffix : substr($filename, -4);
     switch ($suffix) {
      case '.png':
-       $success = imagecreatefrompng($image, $path);
+       return imagecreatefrompng($path);
        break;
      case '.jpg':
-       $success = imagecreatefromjpeg($image, $path);
+         return imagecreatefromjpeg($path);
        break;
      case '.gif':
-       $success = imagecreatefromgif($image, $path);
+       return imagecreatefromgif($path);
        break;
     }
   }
