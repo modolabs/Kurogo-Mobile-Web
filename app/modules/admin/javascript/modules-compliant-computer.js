dde1c39e5e28bced80c763fa7af2c8b596012671
Pete Akins <pete.akins@modolabs.com>
2011-03-22 10:59:59 -0400

    added modules list javascript

---
+++ app/modules/admin/javascript/modules-compliant-computer.js
@@ -0,0 +1,10 @@
+    
+$(document).ready(function() {
+    
+    $('#adminForm').submit(function(e) {
+        alert("You didn't think I actually got it all done, did you?");
+        return false;
+    });
+    
+});
+
