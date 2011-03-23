b662ccd9b1576cffcd64fc0dfcd100dc89087f12
Pete Akins <pete.akins@modolabs.com>
2011-03-23 13:41:29 -0400

    fixed javascript error

--- app/modules/admin/javascript/compliant-computer.js
+++ app/modules/admin/javascript/compliant-computer.js
@@ -21,30 +21,30 @@ function createFormFieldListItem(fieldData) {
     switch (fieldData.type) {
     
         case 'time':
-            li.append($('<input/>').attr('type','text').attr('name', fieldData.key).attr('section', fieldData.section).attr('config', fieldData.config).attr('value', fieldData.value).attr('class','timeData'));
+            li.append($('<input/>').attr('type','text').attr('name', fieldData.key).attr('section', section).attr('config', fieldData.config).attr('value', fieldData.value).attr('class','timeData'));
             li.append('seconds');
             break;
         case 'file':
             li.append(createSelectBox(fileListTypes(), fieldData.constant).attr('class','filePrefix'));
-            li.append($('<input/>').attr('type','text').attr('name', fieldData.key).attr('section', fieldData.section).attr('config', fieldData.config).attr('value', fieldData.value).attr('class','fileData'));
+            li.append($('<input/>').attr('type','text').attr('name', fieldData.key).attr('section', section).attr('config', fieldData.config).attr('value', fieldData.value).attr('class','fileData'));
             break;
         case 'number':
-            li.append($('<input/>').attr('type','text').attr('name', fieldData.key).attr('section', fieldData.section).attr('config', fieldData.config).attr('value', fieldData.value));
+            li.append($('<input/>').attr('type','text').attr('name', fieldData.key).attr('section', section).attr('config', fieldData.config).attr('value', fieldData.value));
             break;
         case 'password':
         case 'text':
-            li.append($('<input/>').attr('type',fieldData.type).attr('name', fieldData.key).attr('section', fieldData.section).attr('config', fieldData.config).attr('value', fieldData.value));
+            li.append($('<input/>').attr('type',fieldData.type).attr('name', fieldData.key).attr('section', section).attr('config', fieldData.config).attr('value', fieldData.value));
             break;
         case 'checkbox':
-            li.append($('<input/>').attr('type','hidden').attr('name', fieldData.key).attr('section', fieldData.section).attr('config', fieldData.config).attr('value', '0'));
-            li.append($('<input/>').attr('type',fieldData.type).attr('name', fieldData.key).attr('section', fieldData.section).attr('config', fieldData.config).attr('value', '1').attr('checked', parseInt(fieldData.value) ? 'checked':''));
+            li.append($('<input/>').attr('type','hidden').attr('name', fieldData.key).attr('section', section).attr('config', fieldData.config).attr('value', '0'));
+            li.append($('<input/>').attr('type',fieldData.type).attr('name', fieldData.key).attr('section', section).attr('config', fieldData.config).attr('value', '1').attr('checked', parseInt(fieldData.value) ? 'checked':''));
             break;
         case 'select':
             var options = 'options' in fieldData ? fieldData.options : [];
-            li.append(createSelectBox(options, fieldData.value).attr('name',fieldData.key).attr('section', fieldData.section).attr('config', fieldData.config));
+            li.append(createSelectBox(options, fieldData.value).attr('name',fieldData.key).attr('section', section).attr('config', fieldData.config));
             break;
         case 'paragraph':
-            li.append($('<textarea>'+(fieldData.value ? fieldData.value : '')+'</textarea>').attr('name',fieldData.key).attr('rows','5').attr('section', fieldData.section).attr('config', fieldData.config));
+            li.append($('<textarea>'+(fieldData.value ? fieldData.value : '')+'</textarea>').attr('name',fieldData.key).attr('rows','5').attr('section', section).attr('config', fieldData.config));
             break;
         case 'label':
             li.append(fieldData.value);
