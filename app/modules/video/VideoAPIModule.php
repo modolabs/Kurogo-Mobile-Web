4e2e6b85f4e902a4f1b21ce5440cf908f16170d0
Jim Kang <jim.kang@modolabs.com>
2011-03-30 14:46:48 -0400

    Added video API module. Factored getListItemForVideo and getSections out of VideoWebModule and into VideoModuleUtils.

---
+++ app/modules/video/VideoAPIModule.php
@@ -0,0 +1,61 @@
+<?php
+
+includePackage('Video');
+
+class VideoAPIModule extends APIModule {    
+    protected $id='video';  // this affects which .ini is loaded
+    protected $feeds = array();
+    
+    public static function cleanVideoArray($videoArray) {
+        $cleanArray = array();
+        foreach ($videoArray as $key => $value)
+        {
+            $cleanKey = ltrim($key, "\0*");
+            $cleanArray[$cleanKey] = $value;
+        }
+        //error_log(print_r($cleanArray, true));
+        return $cleanArray;
+    }
+            
+    public function initializeForCommand() {
+        $this->feeds = $this->loadFeedData();
+        
+        switch ($this->command) {
+            case 'sections':
+                error_log(print_r(VideoModuleUtils::getSectionsFromFeeds($this->feeds), true));
+                $this->setResponse(VideoModuleUtils::getSectionsFromFeeds($this->feeds));
+                $this->setResponseVersion(1);                
+                break;
+            case 'videos':
+            case 'search':            
+                // videos commands requires one argument: section.
+                // search requires two arguments: section and q (query).
+                $section = $this->getArg('section');
+                $query = $this->getArg('q');                
+                
+                $feedData = $this->feeds[$section];
+                $controller = DataController::factory($feedData['CONTROLLER_CLASS'], $feedData);
+                $totalItems = $controller->getTotalItems();
+                $videos = array();
+                
+                if ($this->command == 'search') {
+                    $items = $controller->search($query, 0, 20);
+                }
+                else {
+                    $items = $controller->items(0, 50);
+                }
+                
+                foreach ($items as $video) {
+                    $videoItem = VideoModuleUtils::getListItemForVideo($video, $section);
+                    $videoItem['video'] = VideoAPIModule::cleanVideoArray((array)$video);
+                    $videos[] = $videoItem;
+                }
+                $this->setResponse($videos);
+                $this->setResponseVersion(1);                
+                break;            
+            default:
+                $this->invalidCommand();
+                break;
+        }
+    }
+}
