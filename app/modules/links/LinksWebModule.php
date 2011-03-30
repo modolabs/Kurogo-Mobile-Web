e0b0bb7bef2299b09009b18d9ff01c355867bd7e
Pete Akins <pete.akins@modolabs.com>
2011-03-30 16:36:22 -0400

    Unavoidable change in configuration for links module. Makes admin much simpler.

--- app/modules/links/LinksWebModule.php
+++ app/modules/links/LinksWebModule.php
@@ -30,16 +30,14 @@ class LinksWebModule extends WebModule {
   }
   
   protected function initializeForPage() {
-    $links = $this->getModuleArray('links');
-        
-    foreach ($links as $index => $link) {
-      if (!is_array($link)) {
-        unset($links[$index]);
-      } else if (self::argVal($link, 'icon', false)) {
-        $links[$index]['img'] = "/modules/{$this->id}/images/{$link['icon']}{$this->imageExt}";
-      }
-    }
+    $links = $this->getModuleSections('links');
     
+    foreach ($links as $index => &$link) {
+        if (self::argVal($link, 'icon', false)) {
+            $link['img'] = "/modules/{$this->configModule}/images/{$link['icon']}{$this->imageExt}";
+        }
+    }
+        
     $this->assign('displayType', $this->getModuleVar('display_type'));
     $this->assign('description', $this->getModuleVar('description','strings'));
     $this->assign('links',       $links);
