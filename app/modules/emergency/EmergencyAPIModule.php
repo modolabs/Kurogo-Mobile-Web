bbb1fd6fdb130db85a3cd41361d78b972aaa6337
Brian Patt <brian.patt@modolabs.com>
2011-04-03 18:57:39 -0400

    adding the rest api call to retreive the emergency contacts list

--- app/modules/emergency/EmergencyAPIModule.php
+++ app/modules/emergency/EmergencyAPIModule.php
@@ -20,6 +20,33 @@ class EmergencyAPIModule extends APIModule {
                 $this->setResponse($response);
                 $this->setResponseVersion(1);
                 break;
+
+            case 'contacts':
+                $contactsConfig = $config->getSection('contacts');
+                $contactsController = DataController::factory($contactsConfig['CONTROLLER_CLASS'], $contactsConfig);
+
+                $response = array(
+                    'primary' => self::formatContacts($contactsController->getPrimaryContacts()),
+                    'secondary' => self::formatContacts($contactsController->getSecondaryContacts()),
+                );
+                $this->setResponse($response);
+                $this->setResponseVersion(1);
+                break;
+
         }
+    }
+
+
+    protected static function formatContacts($contacts) {
+        $formattedContacts = array();
+        foreach($contacts as $contact) {
+             $formattedContacts[] = array(
+                'title' => $contact->getTitle(),
+                'subtitle' => $contact->getSubtitle(),
+                'formattedPhone' => $contact->getPhoneDelimitedByPeriods(),
+                'dialablePhone' => $contact->getPhoneDialable(),
+             );
+        }
+        return $formattedContacts;
     }
 }
