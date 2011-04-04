f3e49c258ea5fcb537f21ada112603ee9478f042
Pete Akins <pete.akins@modolabs.com>
2011-04-04 16:38:13 -0400

    added ability to turn off tablet ui

--- lib/DeviceClassifier.php
+++ lib/DeviceClassifier.php
@@ -52,6 +52,20 @@ class DeviceClassifier {
       
       if ($data = Kurogo::getSiteVar('MOBI_SERVICE_USE_EXTERNAL') ? 
         $this->detectDeviceExternal($this->userAgent) : $this->detectDeviceInternal($this->userAgent) ) {
+        
+
+        if ($data['pagetype']=='tablet' && !Kurogo::getOptionalSiteVar('TABLET_ENABLED', 1)) {
+            
+            //@TODO make this less hard coded
+            switch ($data['platform'])
+            {
+                case 'ipad':
+                    $data['pagetype'] = 'compliant';  
+                    $data['platform'] = 'iphone';
+                    break;
+            }
+        }        
+        
         $this->pagetype = $data['pagetype'];
         $this->platform = $data['platform'];
         $this->certs = $data['supports_certificate'];
@@ -149,7 +163,7 @@ class DeviceClassifier {
             $data['pagetype'] = 'touch';
           }
           break;
-          
+        
         case 'compliant':
         case 'webkit':
         default:
