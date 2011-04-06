dcfaa161f8504ab4124695a6e25447e9a14e2c89
Brian Patt <brian.patt@modolabs.com>
2011-04-06 12:36:45 -0400

    on second thought the share template had it correct (with escaping the share URL and share remark) instead we make sure the NewsWebModule passing these variables in unescaped

--- app/modules/news/NewsWebModule.php
+++ app/modules/news/NewsWebModule.php
@@ -195,10 +195,10 @@ class NewsWebModule extends WebModule {
         $this->enablePager($content, $this->feed->getEncoding(), $storyPage);
         
         $this->assign('date',          $date);
-        $this->assign('storyURL',      urlencode($story->getLink()));
+        $this->assign('storyURL',      $story->getLink());
         $this->assign('shareEmailURL', $shareEmailURL);
         $this->assign('title',         $story->getTitle());
-        $this->assign('shareRemark',   urlencode($story->getTitle()));
+        $this->assign('shareRemark',   $story->getTitle());
         $this->assign('author',        $story->getAuthor());
         $this->assign('image',         $this->getImageForStory($story));
         break;
