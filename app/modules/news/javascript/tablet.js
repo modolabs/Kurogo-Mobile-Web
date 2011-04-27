0be68a62c2c5a03f22f04fb96ceab8a7fd49a0bf
Pete Akins <pete.akins@modolabs.com>
2011-04-27 09:54:47 -0400

    updated news and video tablet styles

--- app/modules/news/javascript/tablet.js
+++ app/modules/news/javascript/tablet.js
@@ -3,31 +3,35 @@ function moduleInit() {
     newsDetailScroller = new iScroll('storyDetailWrapper', {checkDOMChange: true});
     
     var links = document.querySelectorAll("#stories .results a");
+    var first = true;
     for (var i=0;i<links.length;i++) {
-        links[i].onclick = function(e) {
-            var selected = document.querySelectorAll("#stories .results .newsSelected");
-            for (var j=0;j<selected.length;j++) {
-                selected[j].className=selected[j].oldclass;
-            }
-            this.oldclass = this.className;
-            this.className += ' newsSelected';
-            var httpRequest = new XMLHttpRequest();
-            httpRequest.open("GET", this.href+'&ajax=1', true);
-            httpRequest.onreadystatechange = function() {
-                if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-                    document.getElementById('storyDetail').innerHTML = httpRequest.responseText;
-                    newsDetailScroller.scrollTo(0,0);
-                    newsDetailScroller.refresh();
-                    moduleHandleWindowResize();
+        if (links[i].href.match(/story/)) {
+            links[i].onclick = function(e) {
+                newsDetailScroller.scrollTo(0,0);
+                var selected = document.querySelectorAll("#stories .results .newsSelected");
+                for (var j=0;j<selected.length;j++) {
+                    selected[j].className=selected[j].oldclass;
+                }
+                this.oldclass = this.className;
+                this.className += ' newsSelected';
+                var httpRequest = new XMLHttpRequest();
+                httpRequest.open("GET", this.href+'&ajax=1', true);
+                httpRequest.onreadystatechange = function() {
+                    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+                        document.getElementById('storyDetail').innerHTML = httpRequest.responseText;
+                        newsDetailScroller.refresh();
+                        moduleHandleWindowResize();
+                    }
+                }
+                httpRequest.send(null);
+                if (e) {
+                    e.preventDefault();
                 }
             }
-            httpRequest.send(null);
-            if (e) {
-                e.preventDefault();
+            if (first) {
+                links[i].onclick();
+                first = false;
             }
-        }
-        if (i==0) {
-            links[i].onclick();
         }
     }
 
--- app/modules/video/javascript/tablet.js
+++ app/modules/video/javascript/tablet.js
@@ -3,31 +3,35 @@ function moduleInit() {
     videoDetailScroller = new iScroll('videoDetailWrapper', {checkDOMChange: true} );
     
     var links = document.querySelectorAll("#videos .results a");
+    var first = true;
     for (var i=0;i<links.length;i++) {
-        links[i].onclick = function(e) {
-            var selected = document.querySelectorAll("#videos .results .videoSelected");
-            for (var j=0;j<selected.length;j++) {
-                selected[j].className=selected[j].oldclass;
-            }
-            this.oldclass = this.className;
-            this.className += ' videoSelected';
-            var httpRequest = new XMLHttpRequest();
-            httpRequest.open("GET", this.href+'&ajax=1', true);
-            httpRequest.onreadystatechange = function() {
-                if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-                    document.getElementById('videoDetail').innerHTML = httpRequest.responseText;
-                    videoDetailScroller.scrollTo(0,0);
-                    videoDetailScroller.refresh();
-                    moduleHandleWindowResize();
+        if (links[i].href.match(/detail/)) {
+            links[i].onclick = function(e) {
+                videoDetailScroller.scrollTo(0,0);
+                var selected = document.querySelectorAll("#videos .results .videoSelected");
+                for (var j=0;j<selected.length;j++) {
+                    selected[j].className=selected[j].oldclass;
+                }
+                this.oldclass = this.className;
+                this.className += ' videoSelected';
+                var httpRequest = new XMLHttpRequest();
+                httpRequest.open("GET", this.href+'&ajax=1', true);
+                httpRequest.onreadystatechange = function() {
+                    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+                        document.getElementById('videoDetail').innerHTML = httpRequest.responseText;
+                        videoDetailScroller.refresh();
+                        moduleHandleWindowResize();
+                    }
+                }
+                httpRequest.send(null);
+                if (e) {
+                    e.preventDefault();
                 }
             }
-            httpRequest.send(null);
-            if (e) {
-                e.preventDefault();
+            if (first) {
+                links[i].onclick();
+                first = false;
             }
-        }
-        if (i==0) {
-            links[i].onclick();
         }
     }
     
