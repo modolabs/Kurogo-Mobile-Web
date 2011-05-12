90fdf770e911dfc8821fedbd0b84696c8585616a
sonya huang <sonya.huang@modolabs.com>
2011-05-12 18:45:47 -0400

    support basic search for ArcGIS Server layers

--- lib/Maps/ArcGISParser.php
+++ lib/Maps/ArcGISParser.php
@@ -590,7 +590,7 @@ class ArcGISLayer implements MapFolder, MapListElement {
             'where'          => '',
             'returnGeometry' => 'true',
             'outSR'          => '',
-            'outFields'      => implode(',', array_values($this->fieldNames)),
+            'outFields'      => implode(',', array_keys($this->fieldNames)),
             'f'              => 'json',
         );
         
