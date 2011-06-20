0e2fc7b5edf0e53e7dbb7ec639bce47af2a2214f
Alexandra Ellwood <alexandra.ellwood@modolabs.com>
2011-06-20 15:38:33 -0400

    Set image width and height on the IMG element

--- app/modules/map/MapWebModule.php
+++ app/modules/map/MapWebModule.php
@@ -153,6 +153,9 @@ class MapWebModule extends WebModule {
             $this->assign('zoomInUrl', $this->detailUrlForZoom('in', $imgController));
             $this->assign('zoomOutUrl', $this->detailUrlForZoom('out', $imgController));
 
+            $this->assign('imageWidth',  $imageWidth);
+            $this->assign('imageHeight', $imageHeight);
+
             if (($this->pagetype == 'compliant' && $this->platform != 'bbplus') || $this->pagetype == 'tablet') {
                 $apiURL = FULL_URL_BASE.API_URL_PREFIX."/{$this->configModule}/staticImageURL";
                 $js = <<<JS
