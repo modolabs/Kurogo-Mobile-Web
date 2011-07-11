c1be12baf17b9ffcfc7a89a1e6aa3b9e2d317dca
sonya huang <sonya.huang@modolabs.com>
2011-07-11 15:04:35 -0400

    add dummy class for google places api

---
+++ lib/Maps/GooglePlacesParser.php
@@ -0,0 +1,28 @@
+<?php
+
+class GooglePlacesParser extends DataParser implements MapDataParser {
+    public function parseData($data)
+    {
+        return array();
+    }
+
+    public function getProjection()
+    {
+
+    }
+
+    public function getListItems()
+    {
+        return array();
+    }
+
+    public function getAllFeatures()
+    {
+        return array();
+    }
+
+    public function getChildCategories()
+    {
+        return array();
+    }
+}
