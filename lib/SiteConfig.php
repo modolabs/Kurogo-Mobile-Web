f1f2494ff3d5e9ffa4f1a04c22344cde7bf63155
Pete Akins <pete.akins@modolabs.com>
2011-09-11 09:29:19 -0400

    modified SiteConfig to assist with site detection and constant creation in command line scripts.

--- lib/SiteConfig.php
+++ lib/SiteConfig.php
@@ -22,34 +22,47 @@ class SiteConfig extends ConfigGroup {
     
     //multi site currently only works with a url base of root "/"
     if ($this->getOptionalVar('MULTI_SITE', false, 'kurogo')) {
-        $paths = explode("/", $path); // this is url
-        $sites = array();
-        $siteDir = '';
+
+        // in scripts you can pass the site name to Kurogo::initialize()
+        if (PHP_SAPI == 'cli') {
+        
+            $site = strlen($path)>0 ? $path : $this->getVar('DEFAULT_SITE');
+
+            $siteDir = implode(DIRECTORY_SEPARATOR, array(ROOT_DIR, 'site', $site));
+            if (!realpath_exists($siteDir)) {
+                die("FATAL ERROR: Site Directory $siteDir not found for site $path");
+            }
+        } else {        
+
+            $paths = explode("/", $path); // this is url
+            $sites = array();
+            $siteDir = '';
+        
+            if (count($paths)>1) {
+                $site = $paths[1];
     
-        if (count($paths)>1) {
-            $site = $paths[1];
-
-            if ($sites = $this->getOptionalVar('ACTIVE_SITES', array(), 'kurogo')) {
-                //see if the site is in the list of available sites
-                if (in_array($site, $sites)) {
-                    $siteDir = realpath_exists(implode(DIRECTORY_SEPARATOR, array(ROOT_DIR, 'site', $site)));
-                    $urlBase = '/' . $site . '/'; // this is a url
-                }
-            } elseif ($this->isValidSiteName($site)) {
-
-                $testPath = implode(DIRECTORY_SEPARATOR, array(ROOT_DIR, 'site', $site));
-                if ($siteDir = realpath_exists($testPath)) {
-                    $urlBase = '/' . $site . '/'; // this is a url
+                if ($sites = $this->getOptionalVar('ACTIVE_SITES', array(), 'kurogo')) {
+                    //see if the site is in the list of available sites
+                    if (in_array($site, $sites)) {
+                        $siteDir = realpath_exists(implode(DIRECTORY_SEPARATOR, array(ROOT_DIR, 'site', $site)));
+                        $urlBase = '/' . $site . '/'; // this is a url
+                    }
+                } elseif ($this->isValidSiteName($site)) {
+    
+                    $testPath = implode(DIRECTORY_SEPARATOR, array(ROOT_DIR, 'site', $site));
+                    if ($siteDir = realpath_exists($testPath)) {
+                        $urlBase = '/' . $site . '/'; // this is a url
+                    }
                 }
             }
-        }
-                
-        if (!$siteDir) {
-            $site = $this->getVar('DEFAULT_SITE');
-            array_splice($paths, 1, 1, array($site, $paths[1]));
-            $url = implode("/", $paths);
-            header("Location: $url");
-            die();
+                    
+            if (!$siteDir) {
+                $site = $this->getVar('DEFAULT_SITE');
+                array_splice($paths, 1, 1, array($site, $paths[1]));
+                $url = implode("/", $paths);
+                header("Location: $url");
+                die();
+            }
         }
     } else {
         //make sure active site is set    
@@ -62,27 +75,30 @@ class SiteConfig extends ConfigGroup {
             die("FATAL ERROR: Site Directory ". $this->getVar('SITE_DIR') . " not found for site " . $site);
         }
         
-        //
-        // Get URL base
-        //
-        if ($urlBase = $config->getOptionalVar('URL_BASE','','kurogo')) {
-            $urlBase = rtrim($urlBase,'/').'/';
-        } else {
-            //extract the path parts from the url
-            $pathParts = array_values(array_filter(explode("/", $_SERVER['REQUEST_URI'])));
-            $testPath = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR;
-            $urlBase = '/';
-    
-            //once the path equals the WEBROOT_DIR we've found the base. This only works with symlinks
-              if (realpath($testPath) != WEBROOT_DIR) {
-                foreach ($pathParts as $dir) {
-                      $test = $testPath.$dir.DIRECTORY_SEPARATOR;
-                  
-                    if (realpath_exists($test)) {
-                        $testPath = $test;
-                        $urlBase .= $dir.'/';
-                        if (realpath($test) == WEBROOT_DIR) {
-                            break;
+        if (PHP_SAPI != 'cli') {
+
+            //
+            // Get URL base
+            //
+            if ($urlBase = $config->getOptionalVar('URL_BASE','','kurogo')) {
+                $urlBase = rtrim($urlBase,'/').'/';
+            } else {
+                //extract the path parts from the url
+                $pathParts = array_values(array_filter(explode("/", $_SERVER['REQUEST_URI'])));
+                $testPath = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR;
+                $urlBase = '/';
+        
+                //once the path equals the WEBROOT_DIR we've found the base. This only works with symlinks
+                  if (realpath($testPath) != WEBROOT_DIR) {
+                    foreach ($pathParts as $dir) {
+                          $test = $testPath.$dir.DIRECTORY_SEPARATOR;
+                      
+                        if (realpath_exists($test)) {
+                            $testPath = $test;
+                            $urlBase .= $dir.'/';
+                            if (realpath($test) == WEBROOT_DIR) {
+                                break;
+                            }
                         }
                     }
                 }
@@ -90,22 +106,27 @@ class SiteConfig extends ConfigGroup {
         }
     }
     define('SITE_NAME', $site);
-    if (!isset($urlBase)) {
-        throw new Exception("URL base not set. Please report the configuration to see why this happened");
-    }
-    
-    define('URL_BASE', $urlBase);
 
-    // Strips out the leading part of the url for sites where 
-    // the base is not located at the document root, ie.. /mobile or /m 
-    // Also strips off the leading slash (needed by device debug below)
-    if (isset($path)) {
-        // Strip the URL_BASE off the path
-        $baseLen = strlen(URL_BASE);
-        if ($baseLen && strpos($path, URL_BASE) === 0) {
-            $path = substr($path, $baseLen);
+    if (PHP_SAPI == 'cli') {
+        define('URL_BASE', null);
+    } else {
+        if (!isset($urlBase)) {
+            throw new Exception("URL base not set. Please report the configuration to see why this happened");
         }
-    }  
+        
+        define('URL_BASE', $urlBase);
+    
+        // Strips out the leading part of the url for sites where 
+        // the base is not located at the document root, ie.. /mobile or /m 
+        // Also strips off the leading slash (needed by device debug below)
+        if (isset($path)) {
+            // Strip the URL_BASE off the path
+            $baseLen = strlen(URL_BASE);
+            if ($baseLen && strpos($path, URL_BASE) === 0) {
+                $path = substr($path, $baseLen);
+            }
+        }  
+    }
 
     // Set up defines relative to SITE_DIR
     define('SITE_DIR',             $siteDir); //already been realpath'd
