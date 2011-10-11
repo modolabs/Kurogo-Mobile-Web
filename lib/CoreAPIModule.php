0b6a61da802ff0549ac4fbec1621c4346fe94be5
Pete Akins <pete.akins@modolabs.com>
2011-10-11 16:20:58 -0400

    updated hello api to include module order on home screen

--- lib/CoreAPIModule.php
+++ lib/CoreAPIModule.php
@@ -24,15 +24,25 @@ class CoreAPIModule extends APIModule
             case 'hello':
             
                 $allmodules = $this->getAllModules();
+                $homeModules = $this->getModuleNavigationIDs();
                 foreach ($allmodules as $moduleID=>$module) {
                     if (!$module->getModuleVar('disabled', 'module')) {
+                        $home = false;
+                        if ( ($key = array_search($moduleID, $homeModules['primary'])) !== FALSE) {
+                            $home = array('type'=>'primary', 'order'=>$key);
+                        } elseif (($key = array_search($moduleID, $homeModules['secondary'])) !== FALSE) {
+                            $home = array('secondary', $key);
+                        }
+                        
+                    
                         $modules[] = array(
                             'id'        =>$module->getID(),
                             'tag'       =>$module->getConfigModule(),
                             'title'     =>$module->getModuleVar('title','module'),
                             'access'    =>$module->getAccess(AccessControlList::RULE_TYPE_ACCESS),
                             'vmin'      =>$module->getVmin(),
-                            'vmax'      =>$module->getVmax()
+                            'vmax'      =>$module->getVmax(),
+                            'home'      =>$home
                         );
                     }
                 }
