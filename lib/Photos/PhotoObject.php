343a0a4465e79eae8095c808d6ad9fb6c5b3b53f
Jeffery <i@youjf.com>
2011-11-22 17:04:52 +0800

    use BaseObject as parent Object for PhotoObject

--- lib/Photos/PhotoObject.php
+++ lib/Photos/PhotoObject.php
@@ -1,6 +1,6 @@
 <?php
 
-class PhotoObject implements KurogoObject {
+class PhotoObject extends BaseObject implements KurogoObject {
     protected $id;
     protected $title;
     /**
@@ -78,63 +78,6 @@ class PhotoObject implements KurogoObject {
     protected $height;
     protected $tags;
     
-    /**
-     * __call 
-     * handle all standard set/get functions
-     * 
-     * @param mixed $name 
-     * @param mixed $args 
-     * @access public
-     * @return mixed
-     */
-    public function __call($name, $args) {
-        $action = substr($name, 0, 3);
-        $do = $this->camelDown(substr($name, 3));
-        if(!$do) {
-            return false;
-        }
-        if($action == 'get') {
-            if($this->$do === null) {
-                return '';
-            }
-            return $this->$do;
-        }
-        if($action == 'set') {
-            $this->$do = $args[0];
-        }
-        return false;
-    }
-
-    protected function camelDown($name) {
-        $lowercase = strtolower($name);
-        if(property_exists($this, $lowercase)) {
-            return $lowercase;
-        }
-        $words = array();
-        $word = '';
-        for($i = 0;$i < strlen($name);$i ++) {
-            // if upper case, previous is a word
-            if(ord($name[$i]) < 97) {
-                if(strlen($word)) {
-                    $words[] = strtolower($word);
-                }
-                $word = $name[$i];
-            }else {
-                // new word starts
-                $word .= $name[$i];
-            }
-            // if this is the last character, it is a word too
-            if($i == (strlen($name) - 1)) {
-                $words[] = strtolower($word);
-            }
-        }
-        return implode($words, '_');
-    }
-
-    public function getID() {
-        return $this->id;
-    }
-
     public function setDateTaken(DateTime $date) {
         $this->date_taken = $date;
     }
