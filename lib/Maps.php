9b523a69243ecae2d30ba16a61da2780b5b77c22
sonya huang <sonya.huang@modolabs.com>
2011-11-23 17:09:20 -0500

    remove nonsense exception that was just there to get a backtrace

--- lib/Maps.php
+++ lib/Maps.php
@@ -102,9 +102,6 @@ function normalizedBoundingBox($center, $tolerance, $fromProj=null, $toProj=null
 }
 
 function mapIdForFeedData(Array $feedData) {
-    if (!$feedData['TITLE']) {
-        throw new Exception("asdf");
-    }
     $identifier = $feedData['TITLE'];
     if (isset($feedData['BASE_URL'])) {
         $identifier .= $feedData['BASE_URL'];
