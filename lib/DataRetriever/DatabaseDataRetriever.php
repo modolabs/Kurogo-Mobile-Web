7e6901cfdddc36f7c647311ee4eb638d84d35e6d
Pete Akins <pete.akins@modolabs.com>
2011-12-05 01:24:22 -0500

    allow config based definition of SQL query data

--- lib/DataRetriever/DatabaseDataRetriever.php
+++ lib/DataRetriever/DatabaseDataRetriever.php
@@ -13,6 +13,14 @@ class DatabaseDataRetriever extends DataRetriever
             $args = array_merge(Kurogo::getSiteSection('database'), $args);
         }
         
+        if (isset($args['SQL'])) {
+            $this->setSQL($args['SQL']);
+        }
+
+        if (isset($args['PARAMETERS']) && is_array($args['PARAMETERS'])) {
+            $this->setParameters($args['PARAMETERS']);
+        }
+        
         $this->connection = new db($args);                
     }
 
