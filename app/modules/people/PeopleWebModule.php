88d5f1adc85c486f0fe3cb94b0bfec765dbebf0a
Pete Akins <pete.akins@modolabs.com>
2011-12-15 17:50:44 -0500

    allow showing contacts subtitles on new line

--- app/modules/people/PeopleWebModule.php
+++ app/modules/people/PeopleWebModule.php
@@ -384,6 +384,7 @@ class PeopleWebModule extends WebModule {
                 }
                 $this->assign('placeholder', $this->getLocalizedString("SEARCH"));
                 $this->assign('searchTip', $this->getOptionalModuleVar('SEARCH_TIP'));
+                $this->assign('contactsSubTitleNewline', $this->getOptionalModuleVar('CONTACTS_SUBTITLE_NEWLINE', false));
                 break;
         }  
     }
