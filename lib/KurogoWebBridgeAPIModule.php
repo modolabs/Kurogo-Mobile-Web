6f455a09c3b16db28d272f331fc20cde3a0e8877
Brian Patt <blpatt@mit.edu>
2011-12-15 20:17:26 -0500

    reverting previous commit (7921f9cfd54e0899b6921d437a6b577cdea0a0ec) in retrospect adding vmin and vmax is the wrong thing to do.

--- lib/KurogoWebBridgeAPIModule.php
+++ lib/KurogoWebBridgeAPIModule.php
@@ -1,10 +1,6 @@
 <?php
 
 class KurogoWebBridgeAPIModule extends APIModule {
-
-    protected $vmin = 1;
-    protected $vmax = 1;
-
     public function getWebBridgeConfig() {
         return KurogoWebBridge::getAssetsConfiguration($this->configModule);
     }
