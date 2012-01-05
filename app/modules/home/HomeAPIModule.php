099f5b34d38927e0ad72a14f0beee5085b383700
Pete Akins <pete.akins@modolabs.com>
2012-01-04 20:40:32 -0500

    added notice api to home module

--- app/modules/home/HomeAPIModule.php
+++ app/modules/home/HomeAPIModule.php
@@ -7,7 +7,39 @@ class HomeAPIModule extends APIModule
     protected $vmax = 1;
 
     public function initializeForCommand() {
-        $this->invalidCommand();
+        switch ($this->command)
+        {
+            case 'notice':
+                $response = null;
+                $responseVersion = 1;
+                $notice = $this->getOptionalModuleSection('notice');
+                if ($notice) {
+                    $bannerNotice = null;
+                    // notice can either take a module or data model class or retriever class. The section is passed on. It must implement the HomeAlertInterface interface
+                    if (isset($notice['MODULE'])) {
+                        $moduleID = $notice['MODULE'];
+                        $controller = WebModule::factory($moduleID);
+                    } elseif (isset($notice['MODEL_CLASS'])) {
+                        $controller = DataModel::factory($notice['MODEL_CLASS'], $notice);
+                    } elseif (isset($notice['RETRIEVER_CLASS'])) {
+                        $controller = DataRetriever::factory($notice['RETRIEVER_CLASS'], $notice);
+                    }
+    
+                    if (!$controller instanceOf HomeAlertInterface) {
+                        throw new KurogoConfigurationException("Module $moduleID does not implement HomeAlertModule interface");
+                    } 
+    
+                    if ($bannerNotice = $controller->getHomeScreenAlert()) {
+                        $response = $bannerNotice;
+                    }
+                }
+
+                $this->setResponse($response);
+                $this->setResponseVersion($responseVersion);
+                break;
+            default:
+                $this->invalidCommand();
+        }
     }
 }
 
