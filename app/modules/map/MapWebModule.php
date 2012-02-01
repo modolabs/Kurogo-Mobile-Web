099ce24a5b48b9347b2964ba244899ff36250027
sonya huang <sonya.huang@modolabs.com>
2012-02-01 14:09:34 -0500

    make sure the map/list toggle on category page shows list option when in map view

--- app/modules/map/MapWebModule.php
+++ app/modules/map/MapWebModule.php
@@ -779,9 +779,15 @@ class MapWebModule extends WebModule {
 
                 // link to "view all on map"
                 $mapArgs = $this->args;
-                $mapArgs['mapview'] = true;
-                $mapURL = $this->buildBreadcrumbURL($this->page, $mapArgs, false);
-                $this->assign('mapURL', $mapURL);
+                if (isset($mapArgs['mapview']) && $mapArgs['mapview']) {
+                    unset($mapArgs['mapview']);
+                    $browseURL = $this->buildBreadcrumbURL($this->page, $mapArgs, false);
+                    $this->assign('browseURL', $browseURL);
+                } else {
+                    $mapArgs['mapview'] = true;
+                    $mapURL = $this->buildBreadcrumbURL($this->page, $mapArgs, false);
+                    $this->assign('mapURL', $mapURL);
+                }
 
                 break;
           
