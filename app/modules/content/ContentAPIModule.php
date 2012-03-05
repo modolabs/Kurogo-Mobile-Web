2799664303b3c1feb3d330c626833ae194ae0744
Pete Akins <pete.akins@modolabs.com>
2012-03-05 16:09:01 -0500

    Updated to content module and API

--- app/modules/content/ContentAPIModule.php
+++ app/modules/content/ContentAPIModule.php
@@ -5,13 +5,61 @@ class ContentAPIModule extends APIModule {
     protected $id = 'content';
     protected $vmin = 1;
     protected $vmax = 1;
+    protected $feedGroups = null;
 	protected static $defaultModel = 'ContentDataModel';
+	// overrides function in Module.php
+	protected function loadFeedData($group=null) {	
+		if(!$group){
+			$items = $this->getModuleSections('feeds');            
+			foreach ($items as $key => &$item) {
+				if (isset($item['GROUP']) && strlen($item['GROUP'])) {
+					$groupData = $this->getDataForGroup($item['GROUP']);
+					if (!isset($item['TITLE']) && isset($groupData['TITLE'])) {
+						$item['TITLE'] = $groupData['TITLE'];
+					}
+				}
+				$item['title'] = $item['TITLE'];
+			}
+		}else{
+			$items = $this->getItemsForGroup($group);
+			foreach($items as $key => &$item){
+				if (!isset($item['TITLE']) && isset($groupData['TITLE'])) {
+					$item['title'] = $groupData['TITLE'];
+				}else{
+					$item['title'] = $item['TITLE'];
+				}
+			}
+		}
 
+        return $items;
+    }
+	protected function getDataForGroup($group) {
+        if (!$this->feedGroups) {
+             $this->feedGroups = $this->getFeedGroups();
+        }
 
+		if (isset($this->feedGroups[$group])) {
+
+            if (!isset($this->feedGroups[$group]['DESCRIPTION'])) {
+                $this->feedGroups[$group]['DESCRIPTION'] = $this->getOptionalModuleVar('description','','strings');
+            }
+            
+            return $this->feedGroups[$group];            
+        } else {
+            throw new KurogoConfigurationException("Unable to find link group information for $group");
+        }
+    }    
+    public function getFeedGroups() {
+        return $this->getModuleSections('feedgroups');
+    }
+
+	public function getItemsForGroup($group){
+		return $this->getModuleSections('feeds-'.$group);
+	}
     // From ContentWebModule.php
     protected function getContent($feedData) {
 
-        $content_type = isset($feedData['CONTENT_TYPE']) ? $feedData['CONTENT_TYPE'] : '';
+        $content_type = isset($feedData['CONTENT_TYPE']) ? $feedData['CONTENT_TYPE'] : 'html_url';
         $modelClass = isset($feedData['MODEL_CLASS']) ? $feedData['MODEL_CLASS'] : self::$defaultModel;
 
         switch ($content_type)
@@ -34,10 +82,16 @@ class ContentAPIModule extends APIModule {
                     $content = $controller->getContentById($feedData['HTML_ID']);
                 } elseif (isset($feedData['HTML_TAG']) && strlen($feedData['HTML_TAG'])>0) {
                     $content = $controller->getContentByTag($feedData['HTML_TAG']);
+                } elseif (isset($feedData['HTML_CLASS']) && strlen($feedData['HTML_CLASS'])>0) {
+                    $content = $controller->getContentByClass($feedData['HTML_CLASS']);
+                } elseif (isset($feedData['KUROGO_READER']) && $feedData['KUROGO_READER']) {
+                    $reader = new KurogoReader($feedData['BASE_URL']);
+                    $content = $reader->getContent();
                 } else {
                     $content = $controller->getContent();
                 }
                 
+                $content = $this->filterContentByPage($content, $this->pageId, $this->group);
                 return $content;
                 break;
             case 'rss':
@@ -57,6 +111,9 @@ class ContentAPIModule extends APIModule {
         }
     }
 
+    protected function filterContentByPage($content, $page, $group) {
+    }
+
      protected function initializeForCommand() {
         if (!$feeds = $this->loadFeedData()) {
 
@@ -64,48 +121,85 @@ class ContentAPIModule extends APIModule {
         }
 
         switch ($this->command) {
-            case 'feeds': // pre 1.0
-            case 'pages': // 1.0
+            case 'feeds': 
+            case 'pages': 
+            	$group = $this->getArg('group', false);
                 $pages = array();
-
-                foreach ($feeds as $page => $feedData) {
-                    $pages[] = array(
-                        'key' => $page,
-                        'title' => $feedData['TITLE'],
-                        'subtitle' => isset($feedData['SUBTITLE']) ? $feedData['SUBTITLE'] : '',
-                        'showTitle' => isset($feedData['SHOW_TITLE']) ? $feedData['SHOW_TITLE'] : false,
-                        //'url' => isset($feedData['BASE_URL']) ? $feedData['BASE_URL'] : ''//$this->buildBreadCrumbURL($page, array())
-                    );
-                }
-
-                $response = array(
-                    'totalFeeds' => count($feeds),
-                    'pages' => $pages,
-                );
-
+                
+				if(isset($group)  && $group){
+					// return specified group 
+					$feeds = $this->loadFeedData($group);
+					foreach ($feeds as $page => $feedData) {
+		                    $pages[] = array(
+		                        'key' => $page,
+		                        'title' => $feedData['TITLE'],
+		                        'subtitle' => isset($feedData['SUBTITLE']) ? $feedData['SUBTITLE'] : '',
+		                        'group' => $group,
+		                    );
+					}
+				} else{
+					// return all feeds
+	                foreach ($feeds as $page => $feedData) {
+	                	if(isset($feedData['GROUP'])  && $feedData['GROUP']){
+		                    $pages[] = array(
+		                        'key' => $page,
+		                        'title' => $feedData['TITLE'],
+		                        'subtitle' => isset($feedData['SUBTITLE']) ? $feedData['SUBTITLE'] : '',
+		                        'group' => $feedData['GROUP'],
+		                    );
+	                	} else {
+		                    $pages[] = array(
+		                        'key' => $page,
+		                        'title' => $feedData['TITLE'],
+		                        'subtitle' => isset($feedData['SUBTITLE']) ? $feedData['SUBTITLE'] : '',
+		                        'showTitle' => isset($feedData['SHOW_TITLE']) ? $feedData['SHOW_TITLE'] : false,
+		                        //'url' => isset($feedData['BASE_URL']) ? $feedData['BASE_URL'] : ''//$this->buildBreadCrumbURL($page, array())
+		                    );
+	                	}
+	                }
+				}
+	            
+				$response = array(
+						'totalFeeds'	=> count($pages),
+						'pages'	=> $pages,
+					
+				);
                 $this->setResponse($response);
                 $this->setResponseVersion(1);
 
                 break;
-
-            case 'page': // 1.0
-            case 'getFeed': // pre 1.0
-
-                $filter = $this->getArg('key');
-                if (strlen($filter)) {
-                    $feedData = $feeds[$filter];
-                    $feedBody = $this->getContent($feedData);
-
-                    $this->setResponse($feedBody);
-                    $this->setResponseVersion(1);
+            case 'groups':
+				$feedGroups = $this->getModuleSections('feedgroups');
+				
+                foreach ($feedGroups as $gourpName => $gourpData) {
+	                    $groups[] = array(
+	                        'key' => $gourpName,
+	                        'title' => $gourpData['TITLE'],
+	                        'description' => isset($gourpData['DESCRIPTION']) ? $gourpData['DESCRIPTION'] : '',
+	                    );
                 }
-                else {
-                    $this->invalidCommand();
-                    $this->setResponseVersion(1);
-                }
-
-                break;
-
+                
+                $response = array(
+                		'groups'	=> $groups,
+                );
+                $this->setResponse($response);
+                $this->setResponseVersion(1);
+            	break;
+            case 'page': 
+            case 'getFeed': 
+        		$pageId = $this->getArg('key');
+				$group = $this->getArg('group', false);
+				$items = ($group === false) ? $this->loadFeedData() : $this->loadFeedData($group);
+				
+				$feedData = $items[$pageId];
+				
+				$title = $feedData['TITLE']?$feedData['TITLE']:'';
+				
+				$feedbody = $this->getContent($feedData);
+				
+                $this->setResponse($feedbody);
+                $this->setResponseVersion(1);
+				break;
             default:
                 $this->invalidCommand();
                 $this->setResponseVersion(1);
