cdbe8ba23de4944d496cc824930cd808747277ed
Pete Akins <pete.akins@modolabs.com>
2012-03-27 14:21:51 -0400

    use configurable options for user metadata retrieval

--- lib/Authentication/ShibbolethAuthentication.php
+++ lib/Authentication/ShibbolethAuthentication.php
@@ -3,23 +3,26 @@
 class ShibbolethAuthentication extends AuthenticationAuthority
 {
     protected $userClass = 'ShibbolethUser';
+    protected $fieldMap=array();
+
     protected function auth($login, $password, &$user) {
-        if (isset($_SERVER['REMOTE_USER']) && !empty($_SERVER['REMOTE_USER'])) {
-            $user = new $this->userClass($this);
-            $user->setUserID($_SERVER['REMOTE_USER']);
+
+        $user = new $this->userClass($this);
+        if ($user->loadUserData()) {
             return AUTH_OK;
         }
         
+        $user = null;
         return AUTH_FAIL;
     }
 
     public function getUser($login) {
-        if (isset($_SERVER['REMOTE_USER']) && $_SERVER['REMOTE_USER'] == $login) {
-            $user = new $this->userClass($this);
-            $user->setUserID($_SERVER['REMOTE_USER']);
-            return $user;        
+        $user = new $this->userClass($this);
+        $user->loadUserData();
+        if ($user->getUserID() == $login) {
+            return $user;
         }
-        
+
         return false;
     }
 
@@ -29,10 +32,46 @@ class ShibbolethAuthentication extends AuthenticationAuthority
 
     public function validate(&$error) {
        return true;
-    }   
+    }
 
+    public function getField($field)
+    {
+        return isset($this->fieldMap[$field]) ? $this->fieldMap[$field] : null;
+    }
+    
+    public function init($args) {
+        parent::init($args);
+
+        // set field map using SHIB_XXX_FIELD = "" maps to $_SERVER values
+        foreach ($args as $arg=>$value) {
+            if (preg_match("/^shib_(email|firstname|lastname|fullname)_field$/", strtolower($arg), $bits)) {
+                $key = strtolower($bits[1]);
+                $this->fieldMap[$key] = $value;
+            }
+        }
+    }
 }
 
 class ShibbolethUser extends User
 {
+    public function loadUserData() {
+        if (isset($_SERVER['REMOTE_USER'])) {
+            $this->setUserID($_SERVER['REMOTE_USER']);
+            if ( ($field = $this->AuthenticationAuthority->getField('email')) && isset($_SERVER[$field])) {
+                $this->setEmail($_SERVER[$field]);
+            }
+            
+            if ( ($field = $this->AuthenticationAuthority->getField('fullname')) && isset($_SERVER[$field])) {
+                $this->setFullName($_SERVER[$field]);
+            }
+
+            if ( ($field = $this->AuthenticationAuthority->getField('firstname')) && isset($_SERVER[$field])) {
+                $this->setFirstName($_SERVER[$field]);
+            }
+
+            if ( ($field = $this->AuthenticationAuthority->getField('lastname')) && isset($_SERVER[$field])) {
+                $this->setLastName($_SERVER[$field]);
+            }
+        }
+    }
 }
