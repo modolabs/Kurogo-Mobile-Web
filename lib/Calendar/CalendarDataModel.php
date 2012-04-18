022a76438f79756edc65efe2529c22a5bda91f2e
Wolf Wang <wolfcs@163.com>
2012-04-18 16:49:47 +0800

    KGO-775: fixed code error.

--- lib/Calendar/CalendarDataModel.php
+++ lib/Calendar/CalendarDataModel.php
@@ -264,9 +264,9 @@ class CalendarDataModel extends ItemListDataModel
         $startTimestamp = $this->startTimestamp() ? $this->startTimestamp() : CalendarDataController::START_TIME_LIMIT;
         $endTimestamp = $this->endTimestamp() ? $this->endTimestamp() : CalendarDataController::END_TIME_LIMIT;
         $range = new TimeRange($startTimestamp, $endTimestamp);
+        $events = $calendar->getEventsInRange($range, $this->getLimit(), $this->filters);
         //set total items number
         $this->setTotalItems(count($events));
-        $events = $calendar->getEventsInRange($range, $this->getLimit(), $this->filters);
         return $this->limitItems($events, $this->getStart(), $this->getLimit());
     }
     
