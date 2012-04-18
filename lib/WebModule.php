f503952c8e8245d86bc60052ce0c93578ef77e33
Alexandra Ellwood <alexandra.ellwood@modolabs.com>
2012-04-18 15:08:38 -0400

    [KGO-788] Make robots.txt a dynamic file based on modules settings. Remove google-specific syntax.

--- lib/WebModule.php
+++ lib/WebModule.php
@@ -69,6 +69,7 @@ abstract class WebModule extends Module {
   protected $canBeRemoved = true;
   protected $canBeDisabled = true;
   protected $canBeHidden = true;
+  protected $canAllowRobots = true;
   protected $hideFooterLinks = false;
   
   //
@@ -515,7 +516,7 @@ abstract class WebModule extends Module {
   protected function moduleDisabled() {
     $this->redirectToModule('error', '', array_merge($this->getArrayForRequest(), array('code'=>'disabled')));
   }
-  
+
     public static function getAllThemes() {
         $themes = array();
         $d = dir(SITE_DIR . "/themes");
@@ -560,6 +561,18 @@ abstract class WebModule extends Module {
         }
         
         return $modules;
+    }
+    
+    public function allowRobots() {
+        if (!$this->canAllowRobots) {
+            return false;
+        } else {
+            return $this->getOptionalModuleVar('robots', 1) ? true : false;
+        }
+    }
+    
+    public function canAllowRobots() {
+        return $this->canAllowRobots;
     }
     
     public function canBeAddedToHomeScreen() {
