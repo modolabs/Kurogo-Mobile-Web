831c8d25a032fb8a5cf37a719ed3f266768baa1b
Alexandra Ellwood <alexandra.ellwood@modolabs.com>
2012-05-08 11:10:27 -0400

    Updated comments to help people understand native template code.

--- site/Universitas/app/modules/test/TestWebModule.php
+++ site/Universitas/app/modules/test/TestWebModule.php
@@ -5,18 +5,19 @@ class TestWebModule extends NewsWebModule {
     
     protected function initializeForNativeTemplatePage() {
         // Native template support
-        // specify anything that goes into the header or footer here
-        // and force the appearance of assets so they get loaded into the template
+        // force the appearance of assets so they get loaded into the template
         switch ($this->page) {
             case 'index':
-                // force appearance of section select button
+                // section selection control with search button:
                 $this->assign('sections', array(1, 2));
             case 'search':
+                // ellipsizer javascript on search and index pages:
                 $this->addInternalJavascript('/common/javascript/lib/ellipsizer.js');
                 $this->addOnLoad('setupNewsListing();');
                 break;
                 
             case 'story':
+                // conditional share button image:
                 $this->assign('shareTitle', $this->getLocalizedString('SHARE_THIS_STORY'));
                 $this->assign('shareEmailURL', 'dummy');
                 $this->assign('shareRemark',   'dummy');
