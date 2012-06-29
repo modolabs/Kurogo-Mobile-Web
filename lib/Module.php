22efc8af91380df6ae0e4f9e949985dabe733ef9
Ebrahim Kobeissi <eebs@eebs-tech.com>
2012-06-29 15:07:01 -0400

    Added getOptionalLocalizedString

--- lib/Module.php
+++ lib/Module.php
@@ -710,6 +710,26 @@ abstract class Module
         
         return isset($this->strings[$lang][$key]) ? $this->processString($this->strings[$lang][$key], $opts) : null;
     }
+
+    public function getOptionalLocalizedString($key, $default = null){
+        $args = func_get_args();
+        // Remove default
+        if (isset($args[1])) {
+          unset($args[1]);
+          $args = array_values($args);
+        }
+
+        try {
+          $value = call_user_func_array(array($this, 'getLocalizedString'), $args);
+        } catch (KurogoKeyNotFoundException $e) {
+          if($default !== NULL){
+            $value = $default;
+          }else{
+            $value = $key;
+          }
+        }
+        return $value;
+    }
     
     public function getLocalizedString($key) {
         if (!preg_match("/^[a-z0-9_]+$/i", $key)) {
@@ -733,7 +753,7 @@ abstract class Module
             }
         }
         
-        throw new KurogoConfigurationException("Unable to find string $key for Module $this->id");
+        throw new KurogoKeyNotFoundException("Unable to find string $key for Module $this->id");
     }
     
     /**
