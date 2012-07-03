a298b39675ba7a103e35a0b605492de4f55eafb6
Jeffery <i@youjf.com>
2012-07-03 18:27:50 +0800

    KGO-1315: create new simpleXML parser

---
+++ lib/DataParser/SimpleXMLDataParser.php
@@ -0,0 +1,19 @@
+<?php
+
+class SimpleXMLDataParser extends DataParser {
+    public function parseData($data) {
+        $xml = new SimpleXMLElement($data);
+        return $this->toArray($xml);
+    }
+
+    protected function toArray(SimpleXMLElement $xml) {
+        $array = json_decode(json_encode($xml), TRUE);
+        
+        foreach ( array_slice($array, 0) as $key => $value ) {
+            if ( empty($value) ) $array[$key] = NULL;
+            elseif ( is_array($value) ) $array[$key] = $this->toArray($value);
+        }
+
+        return $array;
+    }    
+}
