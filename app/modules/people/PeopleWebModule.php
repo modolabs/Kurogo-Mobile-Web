3f9e84bbef38a254ba706a4607b12beb4310459d
Pete Akins <pete.akins@modolabs.com>
2012-07-03 22:59:14 -0400

    changed config name for page length

--- app/modules/people/PeopleWebModule.php
+++ app/modules/people/PeopleWebModule.php
@@ -348,7 +348,7 @@ class PeopleWebModule extends WebModule {
                     $this->assign('searchTerms', $searchTerms);
           
                     $startIndex = $this->getArg('start', 0);
-                    $limit = $this->getOptionalModuleVar('SHOW_PEOPLE_PER_PAGE', 20);
+                    $limit = $this->getOptionalModuleVar('MAX_PER_PAGE', 20);
                     $PeopleController->setStart($startIndex);
                     $PeopleController->setLimit($limit);
           
