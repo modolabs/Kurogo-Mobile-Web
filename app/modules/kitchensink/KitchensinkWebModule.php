bc84315e1351f736c40ddf8fd31378d47dec83a7
Alexandra Ellwood <alexandra.ellwood@modolabs.com>
2012-07-09 09:58:31 -0400

    Remove appq test module Clean up kitchensink module appq support.

--- app/modules/kitchensink/KitchensinkWebModule.php
+++ app/modules/kitchensink/KitchensinkWebModule.php
@@ -176,4 +176,13 @@ class KitchensinkWebModule extends WebModule {
                 break;
         }
     }
+    
+    protected function initializeForNativeTemplatePage() {
+        // Native template support
+        // specify anything that goes into the header or footer here
+        // and force the appearance of assets so they get loaded into the template
+        
+        // All the data in this module is static!
+        $this->initializeForPage();
+    }
 }
