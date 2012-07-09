62bc517bdb8f34769c43c9c580bf26c5199e16c5
Pete Akins <pete.akins@modolabs.com>
2012-07-09 18:13:21 -0400

    KGO-1323 started v2 of LoginAPIModule to support multiple identities

--- app/modules/login/LoginAPIModule.php
+++ app/modules/login/LoginAPIModule.php
@@ -4,9 +4,9 @@ class LoginAPIModule extends APIModule
 {
     protected $id = 'login';
     protected $vmin = 1;
-    protected $vmax = 1;
+    protected $vmax = 2;
     public function availableVersions() {
-        return array(1);
+        return array(1,2);
     }
 
     protected function getAccessControlLists($type) {
@@ -56,22 +56,39 @@ class LoginAPIModule extends APIModule
                 
            case 'session':
                 $session = $this->getSession();
-                $user = $this->getUser();
-                
-                $response = array(
-                    'session_id'=>$session->getSessionID(),
-                    'token'=>$session->getLoginToken(),
-                    'user'=>array(
-                        'authority'=>$user->getAuthenticationAuthorityIndex(),
-                        'userID'=>$user->getUserID(),
-                        'name'=>$user->getFullName(),
-                        'sessiondata'=>$user->getSessionData()
-                    )
-                        
-                );
+				$response = array(
+					'session_id'=>$session->getSessionID(),
+					'token'=>$session->getLoginToken(),
+				);
+				
+				// version 2 implements multiple identities into the response
+                if ($this->requestedVersion == 2) {
+                	$response['users'] = array();
+                	$users = $session->getUsers();
+                	foreach ($users as $user) {
+                		$authority = $user->getAuthenticationAuthority();
+                		$response['users'][$authority->getAuthorityIndex()] = array(
+							'authority'=>$authority->getAuthorityIndex(),
+							'authorityTitle'=>$authority->getAuthorityTitle(),
+							'userID'=>$user->getUserID(),
+							'name'=>$user->getFullName(),
+							'sessiondata'=>$user->getSessionData()
+                		);
+                	}
+					$this->setResponseVersion(2);
+                } else {
+					// version 1 assumes only 1 user
+					$user = $this->getUser();
+					$response['user'] = array(
+						'authority'=>$user->getAuthenticationAuthorityIndex(),
+						'userID'=>$user->getUserID(),
+						'name'=>$user->getFullName(),
+						'sessiondata'=>$user->getSessionData()
+					);
+					$this->setResponseVersion(1);
+				}
 
                 $this->setResponse($response);
-                $this->setResponseVersion(1);
                 break;
                 
             default:
