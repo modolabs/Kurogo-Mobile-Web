ae10e5375b9f40f376415fc609b208a364f7b5fe
Alexandra Ellwood <alexandra.ellwood@modolabs.com>
2012-07-11 14:20:23 -0400

    [KGO-1375]
    License banner for all source files

--- lib/Cache/MemcacheCache.php
+++ lib/Cache/MemcacheCache.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * Copyright © 2010 - 2012 Modo Labs Inc. All rights reserved.
+ *
+ * The license governing the contents of this file is located in the LICENSE
+ * file located at the root directory of this distribution. If the LICENSE file
+ * is missing, please contact sales@modolabs.com.
+ *
+ */
+
 if (!function_exists('memcache_connect')) {
     throw new KurogoException('MemcacheCache requires the Memcache Extension');
 }
--- lib/KurogoShellDispatcher.php
+++ lib/KurogoShellDispatcher.php
@@ -1,6 +1,15 @@
 #!/usr/bin/php -q
 <?php
 
+/*
+ * Copyright © 2010 - 2012 Modo Labs Inc. All rights reserved.
+ *
+ * The license governing the contents of this file is located in the LICENSE
+ * file located at the root directory of this distribution. If the LICENSE file
+ * is missing, please contact sales@modolabs.com.
+ *
+ */
+
 class KurogoShellDispatcher {
 
     protected $stdin; //standard input stream
